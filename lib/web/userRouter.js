hackmdio/codimd

public profile and links to the profile
Signed-off-by: Claudius <opensource@amenthes.de>

lib/web/userRouter.js
@@ -2,7 +2,9 @@
 
 const Router = require('express').Router
 
-const {generateAvatar} = require('../letter-avatars')
+const { generateAvatar } = require('../letter-avatars')
+const models = require('../models')
+const response = require('../response')
 
 const UserRouter = module.exports = Router()
 
@@ -10,4 +12,32 @@ UserRouter.get('/user/:username/avatar.svg', function (req, res, next) {
   res.setHeader('Content-Type', 'image/svg+xml')
   res.setHeader('Cache-Control', 'public, max-age=86400')
   res.send(generateAvatar(req.params.username))
+})
+
+UserRouter.get('/user/:username', function (req, res, next) {
+  return models.User.findOne({
+    where: {
+      id: req.params.username
+    }
+  }).then(function (user) {
+    if (!user) {
+      return response.errorNotFound(res)
+    }
+
+    models.Note.findAll({
+      where: {
+        ownerId: user.id
+      }
+    }).then(function (notes) {
+      res.render('user.ejs', {
+        title: 'Profile for ' + user.name,
+        notes: notes,
+        contributions: [], // not yet implemented
+        user: user
+      })
+    })
+  }).catch(function (err) {
+    console.error(err)
+    return response.errorInternalError(res)
+  })
 })

hedgedoc/hedgedoc

lib/web/userRouter.js
@@ -1,11 +1,44 @@
 'use strict'
 
 const Router = require('express').Router
+
 const { generateAvatar } = require('../letter-avatars')
+const models = require('../models')
+const response = require('../response')
+const logger = require('../logger')
+
 const UserRouter = module.exports = Router()
 
 UserRouter.get('/user/:username/avatar.svg', function (req, res, next) {
   res.setHeader('Content-Type', 'image/svg+xml')
   res.setHeader('Cache-Control', 'public, max-age=86400')
   res.send(generateAvatar(req.params.username))
+})
+
+UserRouter.get('/user/:username', function (req, res, next) {
+  return models.User.findOne({
+    where: {
+      id: req.params.username
+    }
+  }).then(function (user) {
+    if (!user) {
+      return response.errorNotFound(res)
+    }
+
+    models.Note.findAll({
+      where: {
+        ownerId: user.id
+      }
+    }).then(function (notes) {
+      res.render('user.ejs', {
+        title: 'Profile for ' + user.name,
+        notes: notes,
+        contributions: [], // not yet implemented
+        user: user
+      })
+    })
+  }).catch(function (err) {
+    logger.error(err)
+    return response.errorInternalError(res)
+  })
 })