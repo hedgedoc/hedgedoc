hackmdio/codimd

introducing username and display name over plain name

lib/web/userRouter.js
@@ -18,11 +18,19 @@ UserRouter.get('/user/:username/avatar.svg', function (req, res, next) {
 UserRouter.get('/user/:username', function (req, res, next) {
   return models.User.findOne({
     where: {
-      id: req.params.username
+      $or: [
+        { id: req.params.username },
+        { username: req.params.username }
+      ]
     }
   }).then(function (user) {
     if (!user) {
       return response.errorNotFound(res)
+    }
+
+    if (user.id === req.params.username) {
+      console.warn('user referred to by userid from the outside. Source of the link: %s', req.get('Referrer'))
+      return res.redirect('/user/' + user.username) // TODO serverURL needs to go in front!
     }
 
     models.Note.findAll({

hedgedoc/hedgedoc

Signed-off-by: Claudius <opensource@amenthes.de>

lib/web/userRouter.js
@@ -19,11 +19,19 @@ UserRouter.get('/user/:username/avatar.svg', function (req, res, next) {
 UserRouter.get('/user/:username', function (req, res, next) {
   return models.User.findOne({
     where: {
-      id: req.params.username
+      $or: [
+        { id: req.params.username },
+        { username: req.params.username }
+      ]
     }
   }).then(function (user) {
     if (!user) {
       return response.errorNotFound(res)
+    }
+
+    if (user.id === req.params.username) {
+      logger.warn('user referred to by userid from the outside. Source of the link: %s', req.get('Referrer'))
+      return res.redirect('/user/' + user.username) // TODO serverURL needs to go in front!
     }
 
     models.Note.findAll({