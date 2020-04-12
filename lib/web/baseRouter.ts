621905bc97b011b63dea9e2e3544144c5b58e248
Philip Molares <philip.molares@udo.edu>
2020-04-12 20:44:52 +0200

    added baseRouter.ts
    Signed-off-by: Philip Molares <philip.molares@udo.edu>
    Signed-off-by: David Mehren <dmehren1@gmail.com>

--- lib/web/baseRouter.ts
+++ lib/web/baseRouter.ts
@@ -1,20 +1,22 @@
 import { response } from '../response'
 import { errors } from '../errors'
+import { Router } from 'express'
 
-const Router = require('express').Router
-const baseRouter = module.exports = Router()
+const BaseRouter = Router()
 
 // get index
-baseRouter.get('/', response.showIndex)
+BaseRouter.get('/', response.showIndex)
 // get 403 forbidden
-baseRouter.get('/403', function (req, res) {
+BaseRouter.get('/403', function (req, res) {
   errors.errorForbidden(res)
 })
 // get 404 not found
-baseRouter.get('/404', function (req, res) {
+BaseRouter.get('/404', function (req, res) {
   errors.errorNotFound(res)
 })
 // get 500 internal error
-baseRouter.get('/500', function (req, res) {
+BaseRouter.get('/500', function (req, res) {
   errors.errorInternalError(res)
 })
+
+export { BaseRouter }
