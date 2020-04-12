6d256dd5b645928f42892000e10e7bdf80f71ba4
Yannick Bungers <git@innay.de>
2020-04-12 19:07:26 +0200

    Added Types for csp.ts
    Signed-off-by: Yannick Bungers <git@innay.de>
    Signed-off-by: David Mehren <dmehren1@gmail.com>

--- lib/csp.ts
+++ lib/csp.ts
@@ -1,10 +1,11 @@
 import { config } from './config'
+import { IHelmetContentSecurityPolicyDirectives } from 'helmet'
+import uuid from 'uuid'
+import { NextFunction, Request, Response } from 'express'
 
-var uuid = require('uuid')
+type CSPDirectives = IHelmetContentSecurityPolicyDirectives
 
-var CspStrategy: any = {}
-
-var defaultDirectives = {
+const defaultDirectives = {
   defaultSrc: ['\'self\''],
   scriptSrc: ['\'self\'', 'vimeo.com', 'https://gist.github.com', 'www.slideshare.net', 'https://query.yahooapis.com', '\'unsafe-eval\''],
   // ^ TODO: Remove unsafe-eval - webpack script-loader issues https://github.com/hackmdio/codimd/issues/594
@@ -17,24 +18,80 @@ var defaultDirectives = {
   connectSrc: ['*']
 }
 
-var cdnDirectives = {
+const cdnDirectives = {
   scriptSrc: ['https://cdnjs.cloudflare.com', 'https://cdn.mathjax.org'],
   styleSrc: ['https://cdnjs.cloudflare.com', 'https://fonts.googleapis.com'],
   fontSrc: ['https://cdnjs.cloudflare.com', 'https://fonts.gstatic.com']
 }
 
-var disqusDirectives = {
+const disqusDirectives = {
   scriptSrc: ['https://disqus.com', 'https://*.disqus.com', 'https://*.disquscdn.com'],
   styleSrc: ['https://*.disquscdn.com'],
   fontSrc: ['https://*.disquscdn.com']
 }
 
-var googleAnalyticsDirectives = {
+const googleAnalyticsDirectives = {
   scriptSrc: ['https://www.google-analytics.com']
 }
 
-CspStrategy.computeDirectives = function () {
-  var directives = {}
+function mergeDirectives (existingDirectives: CSPDirectives, newDirectives: CSPDirectives): void {
+  for (const propertyName in newDirectives) {
+    const newDirective = newDirectives[propertyName]
+    if (newDirective) {
+      const existingDirective = existingDirectives[propertyName] || []
+      existingDirectives[propertyName] = existingDirective.concat(newDirective)
+    }
+  }
+}
+
+function mergeDirectivesIf (condition: boolean, existingDirectives: CSPDirectives, newDirectives: CSPDirectives): void {
+  if (condition) {
+    mergeDirectives(existingDirectives, newDirectives)
+  }
+}
+
+function areAllInlineScriptsAllowed (directives: CSPDirectives): boolean {
+  if (directives.scriptSrc) {
+    return directives.scriptSrc.includes('\'unsafe-inline\'')
+  }
+  return false
+}
+
+function getCspNonce (req: Request, res: Response): string {
+  return "'nonce-" + res.locals.nonce + "'"
+}
+
+function addInlineScriptExceptions (directives: CSPDirectives): void {
+  if (!directives.scriptSrc) {
+    directives.scriptSrc = []
+  }
+  directives.scriptSrc.push(getCspNonce)
+  // TODO: This is the SHA-256 hash of the inline script in build/reveal.js/plugins/notes/notes.html
+  // Any more clean solution appreciated.
+  directives.scriptSrc.push('\'sha256-81acLZNZISnyGYZrSuoYhpzwDTTxi7vC1YM4uNxqWaM=\'')
+}
+
+function addUpgradeUnsafeRequestsOptionTo (directives: CSPDirectives): void {
+  if (config.csp.upgradeInsecureRequests === 'auto' && config.useSSL) {
+    directives.upgradeInsecureRequests = true
+  } else if (config.csp.upgradeInsecureRequests === true) {
+    directives.upgradeInsecureRequests = true
+  }
+}
+
+function addReportURI (directives): void {
+  if (config.csp.reportURI) {
+    directives.reportUri = config.csp.reportURI
+  }
+}
+
+export function addNonceToLocals (req: Request, res: Response, next: NextFunction): void {
+  res.locals.nonce = uuid.v4()
+  next()
+}
+
+export function computeDirectives (): CSPDirectives {
+  const directives: CSPDirectives = {}
   mergeDirectives(directives, config.csp.directives)
   mergeDirectivesIf(config.csp.addDefaults, directives, defaultDirectives)
   mergeDirectivesIf(config.useCDN, directives, cdnDirectives)
@@ -47,55 +104,3 @@ CspStrategy.computeDirectives = function () {
   addReportURI(directives)
   return directives
 }
-
-function mergeDirectives (existingDirectives, newDirectives) {
-  for (var propertyName in newDirectives) {
-    var newDirective = newDirectives[propertyName]
-    if (newDirective) {
-      var existingDirective = existingDirectives[propertyName] || []
-      existingDirectives[propertyName] = existingDirective.concat(newDirective)
-    }
-  }
-}
-
-function mergeDirectivesIf (condition, existingDirectives, newDirectives) {
-  if (condition) {
-    mergeDirectives(existingDirectives, newDirectives)
-  }
-}
-
-function areAllInlineScriptsAllowed (directives) {
-  return directives.scriptSrc.indexOf('\'unsafe-inline\'') !== -1
-}
-
-function addInlineScriptExceptions (directives) {
-  directives.scriptSrc.push(getCspNonce)
-  // TODO: This is the SHA-256 hash of the inline script in build/reveal.js/plugins/notes/notes.html
-  // Any more clean solution appreciated.
-  directives.scriptSrc.push('\'sha256-81acLZNZISnyGYZrSuoYhpzwDTTxi7vC1YM4uNxqWaM=\'')
-}
-
-function getCspNonce (req, res) {
-  return "'nonce-" + res.locals.nonce + "'"
-}
-
-function addUpgradeUnsafeRequestsOptionTo (directives) {
-  if (config.csp.upgradeInsecureRequests === 'auto' && config.useSSL) {
-    directives.upgradeInsecureRequests = true
-  } else if (config.csp.upgradeInsecureRequests === true) {
-    directives.upgradeInsecureRequests = true
-  }
-}
-
-function addReportURI (directives) {
-  if (config.csp.reportURI) {
-    directives.reportUri = config.csp.reportURI
-  }
-}
-
-CspStrategy.addNonceToLocals = function (req, res, next) {
-  res.locals.nonce = uuid.v4()
-  next()
-}
-
-module.exports = CspStrategy
