823bd5e4320cff505002180f5c97ec91505f7c66
Philip Molares <philip.molares@udo.edu>
2020-04-12 20:51:27 +0200

    added lib/web/index.ts to let others import all the routers from one place
    Signed-off-by: Philip Molares <philip.molares@udo.edu>
    Signed-off-by: David Mehren <dmehren1@gmail.com>

---
+++ lib/web/index.ts
@@ -0,0 +1,8 @@
+import { AuthRouter } from './auth'
+import { BaseRouter } from './baseRouter'
+import { HistoryRouter } from './historyRouter'
+import { ImageRouter } from './imageRouter'
+import { NoteRouter } from './note/router'
+import { StatusRouter } from './statusRouter'
+
+export { AuthRouter, BaseRouter, HistoryRouter, ImageRouter, NoteRouter, StatusRouter }
