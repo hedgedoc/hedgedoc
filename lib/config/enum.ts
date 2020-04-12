1437cf3ea5b705c6e697a572c76dafa910b13bf4
David Mehren <dmehren1@gmail.com>
2020-04-12 17:46:37 +0200

    config/enum.ts: Refactor enums into interface and object
    Signed-off-by: David Mehren <dmehren1@gmail.com>

--- lib/config/enum.ts
+++ lib/config/enum.ts
@@ -1,14 +1,29 @@
-export enum Environment {
-  development = 'development',
-  production = 'production',
-  test = 'test'
+export interface Environment {
+  development: string;
+  production: string;
+  test: string;
 }
 
-export enum Permission {
-  freely = 'freely',
-  editable = 'editable',
-  limited = 'limited',
-  locked = 'locked',
-  protected = 'protected',
-  private = 'private'
+export const Environment: Environment = {
+  development: 'development',
+  production: 'production',
+  test: 'test'
+}
+
+export interface Permission {
+  freely: string;
+  editable: string;
+  limited: string;
+  locked: string;
+  protected: string;
+  private: string;
+}
+
+export const Permission: Permission = {
+  freely: 'freely',
+  editable: 'editable',
+  limited: 'limited',
+  locked: 'locked',
+  protected: 'protected',
+  private: 'private'
 }
