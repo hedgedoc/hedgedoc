b841b1ca4c7db0cacf1c5bf93ef7f472d6135b7a
Philip Molares <philip.molares@udo.edu>
2020-04-12 20:37:04 +0200

    fixed lib/utils.ts - added more return types - removed module wrapper
    Signed-off-by: Philip Molares <philip.molares@udo.edu>
    Signed-off-by: David Mehren <dmehren1@gmail.com>

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,54 +1,51 @@
-export module Utils {
-  export function isSQLite(sequelize) {
-    return sequelize.options.dialect === 'sqlite'
-  }
+import { Sequelize } from 'sequelize-typescript'
 
-  export function getImageMimeType(imagePath: string) {
-    const fileExtension = /[^.]+$/.exec(imagePath)
-    switch (fileExtension?.[0]) {
-      case 'bmp':
-        return 'image/bmp'
-      case 'gif':
-        return 'image/gif'
-      case 'jpg':
-      case 'jpeg':
-        return 'image/jpeg'
-      case 'png':
-        return 'image/png'
-      case 'tiff':
-        return 'image/tiff'
-      case 'svg':
-        return 'image/svg+xml'
-      default:
-        return undefined
-    }
-  }
+export function isSQLite (sequelize: Sequelize): boolean {
+  return sequelize.options.dialect === 'sqlite'
+}
 
-  // [Postgres] Handling NULL bytes
-  // https://github.com/sequelize/sequelize/issues/6485
-  export function stripNullByte(value) {
-    value = '' + value
-    // eslint-disable-next-line no-control-regex
-    return value ? value.replace(/\u0000/g, '') : value
-  }
-
-  export function processData (data, _default, process?) {
-    if (data === undefined) return data
-    else if (process) {
-      if (data === null) {
-        return _default
-      } else {
-        return process(data)
-      }
-    } else {
-      if (data === null) {
-        return _default
-      } else {
-        return data
-      }
-    }
+export function getImageMimeType (imagePath: string): string | undefined {
+  const fileExtension = /[^.]+$/.exec(imagePath)
+  switch (fileExtension?.[0]) {
+    case 'bmp':
+      return 'image/bmp'
+    case 'gif':
+      return 'image/gif'
+    case 'jpg':
+    case 'jpeg':
+      return 'image/jpeg'
+    case 'png':
+      return 'image/png'
+    case 'tiff':
+      return 'image/tiff'
+    case 'svg':
+      return 'image/svg+xml'
+    default:
+      return undefined
   }
 }
 
+// [Postgres] Handling NULL bytes
+// https://github.com/sequelize/sequelize/issues/6485
+export function stripNullByte (value: string): string {
+  value = '' + value
+  // eslint-disable-next-line no-control-regex
+  return value ? value.replace(/\u0000/g, '') : value
+}
 
-
+export function processData (data, _default, process?) {
+  if (data === undefined) return data
+  else if (process) {
+    if (data === null) {
+      return _default
+    } else {
+      return process(data)
+    }
+  } else {
+    if (data === null) {
+      return _default
+    } else {
+      return data
+    }
+  }
+}
