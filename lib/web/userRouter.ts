ef6632cac94a3824abc0d9df6f748df7a104de15
Philip Molares <philip.molares@udo.edu>
2020-04-13 11:35:03 +0200

    added userRouter.ts - added @types/passport - added @types/archiver - types all req and res arguments - renamed unused argument next to _
    Signed-off-by: Philip Molares <philip.molares@udo.edu>
    Signed-off-by: David Mehren <dmehren1@gmail.com>

--- lib/web/userRouter.ts
+++ lib/web/userRouter.ts
@@ -1,30 +1,33 @@
-'use strict'
+import archiver from 'archiver'
+import async from 'async'
+import { Request, Response, Router } from 'express'
+import { errors } from '../errors'
+import { Note, User } from '../models'
+import { logger } from '../logger'
+import { generateAvatar } from '../letter-avatars'
+import { config } from '../config'
 
-const archiver = require('archiver')
-const async = require('async')
-const Router = require('express').Router
-
-const errors = require('../errors')
-const config = require('../config')
-const models = require('../models')
-const logger = require('../logger')
-const { generateAvatar } = require('../letter-avatars')
-
-const UserRouter = module.exports = Router()
+const UserRouter = Router()
 
 // get me info
-UserRouter.get('/me', function (req, res) {
+UserRouter.get('/me', function (req: Request, res: Response) {
   if (req.isAuthenticated()) {
-    models.User.findOne({
+    if (req.user == null) {
+      return errors.errorInternalError(res)
+    }
+    User.findOne({
       where: {
         id: req.user.id
       }
     }).then(function (user) {
       if (!user) { return errors.errorNotFound(res) }
-      var profile = models.User.getProfile(user)
+      const profile = User.getProfile(user)
+      if (profile == null) {
+        return errors.errorInternalError(res)
+      }
       res.send({
         status: 'ok',
-        id: req.user.id,
+        id: user.id,
         name: profile.name,
         photo: profile.photo
       })
@@ -40,9 +43,12 @@ UserRouter.get('/me', function (req, res) {
 })
 
 // delete the currently authenticated user
-UserRouter.get('/me/delete/:token?', function (req, res) {
+UserRouter.get('/me/delete/:token?', function (req: Request, res: Response) {
   if (req.isAuthenticated()) {
-    models.User.findOne({
+    if (req.user == null) {
+      return errors.errorInternalError(res)
+    }
+    User.findOne({
       where: {
         id: req.user.id
       }
@@ -67,10 +73,13 @@ UserRouter.get('/me/delete/:token?', function (req, res) {
 })
 
 // export the data of the authenticated user
-UserRouter.get('/me/export', function (req, res) {
+UserRouter.get('/me/export', function (req: Request, res: Response) {
   if (req.isAuthenticated()) {
+    if (req.user == null) {
+      return errors.errorInternalError(res)
+    }
     // let output = fs.createWriteStream(__dirname + '/example.zip');
-    let archive = archiver('zip', {
+    const archive = archiver('zip', {
       zlib: { level: 3 } // Sets the compression level.
     })
     res.setHeader('Content-Type', 'application/zip')
@@ -80,25 +89,28 @@ UserRouter.get('/me/export', function (req, res) {
       logger.error('export user data failed: ' + err)
       return errors.errorInternalError(res)
     })
-    models.User.findOne({
+    User.findOne({
       where: {
         id: req.user.id
       }
     }).then(function (user) {
-      models.Note.findAll({
+      if (user == null) {
+        return errors.errorInternalError(res)
+      }
+      Note.findAll({
         where: {
           ownerId: user.id
         }
       }).then(function (notes) {
-        let filenames = {}
+        const filenames = {}
         async.each(notes, function (note, callback) {
-          let basename = note.title.replace(/\//g, '-') // Prevent subdirectories
+          const basename = note.title.replace(/\//g, '-') // Prevent subdirectories
           let filename
-          let suffix = ''
+          let numberOfDuplicateFilename = 0
           do {
-            let seperator = typeof suffix === 'number' ? '-' : ''
-            filename = basename + seperator + suffix + '.md'
-            suffix++
+            const suffix = numberOfDuplicateFilename !== 0 ? '-' + numberOfDuplicateFilename : ''
+            filename = basename + suffix + '.md'
+            numberOfDuplicateFilename++
           } while (filenames[filename])
           filenames[filename] = true
 
@@ -122,8 +134,10 @@ UserRouter.get('/me/export', function (req, res) {
   }
 })
 
-UserRouter.get('/user/:username/avatar.svg', function (req, res, next) {
+UserRouter.get('/user/:username/avatar.svg', function (req: Request, res: Response, _) {
   res.setHeader('Content-Type', 'image/svg+xml')
   res.setHeader('Cache-Control', 'public, max-age=86400')
   res.send(generateAvatar(req.params.username))
 })
+
+export { UserRouter }
