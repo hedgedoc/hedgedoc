2df8e1687b0d1b58e72dd318fcf1d44a4da44efb
Yannick Bungers <git@innay.de>
2020-04-13 13:34:44 +0200

    Refactored google to Typescript
    Signed-off-by: Yannick Bungers <git@innay.de>
    Signed-off-by: David Mehren <dmehren1@gmail.com>

---
+++ lib/web/auth/google/index.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express'
+import { AuthMiddleware } from '../interface'
+import passport from 'passport'
+import * as GoogleStrategy from 'passport-google-oauth20'
+import {config} from '../../../config'
+import { passportGeneralCallback } from '../utils'
+
+const googleAuth = Router()
+
+export const GoogleMiddleware: AuthMiddleware = {
+  getMiddleware (): Router {
+    passport.use(new GoogleStrategy({
+      clientID: config.google.clientID,
+      clientSecret: config.google.clientSecret,
+      callbackURL: config.serverURL + '/auth/google/callback',
+      userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+    }, passportGeneralCallback))
+
+    googleAuth.get('/auth/google', function (req, res, next) {
+      const authOpts: GoogleStrategy.AuthtenticateOptionsGoogle = { scope: ['profile'], hostedDomain: config.google.hostedDomain }
+      passport.authenticate('google', authOpts)(req, res, next)
+    })
+    googleAuth.get('/auth/google/callback',
+      passport.authenticate('google', {
+        successReturnToOrRedirect: config.serverURL + '/',
+        failureRedirect: config.serverURL + '/'
+      })
+    )
+    return googleAuth
+  }
+}
