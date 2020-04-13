1353e61e871350445cf7c37ce3302004498a3418
David Mehren <dmehren1@gmail.com>
2020-04-13 13:16:08 +0200

    auth/gitlab: Migrate to AuthMiddleware
    Signed-off-by: David Mehren <dmehren1@gmail.com>

---
+++ lib/web/auth/gitlab/index.ts
@@ -0,0 +1,41 @@
+import { Router } from 'express'
+import passport from 'passport'
+import { Strategy as GitlabStrategy } from 'passport-gitlab2'
+import { config } from '../../../config'
+import { response } from '../../../response'
+import { AuthMiddleware } from '../interface'
+import { passportGeneralCallback } from '../utils'
+
+export const GitlabMiddleware: AuthMiddleware =
+  {
+    getMiddleware (): Router {
+      const gitlabAuth = module.exports = Router()
+
+      passport.use(new GitlabStrategy({
+        baseURL: config.gitlab.baseURL,
+        clientID: config.gitlab.clientID,
+        clientSecret: config.gitlab.clientSecret,
+        scope: config.gitlab.scope,
+        callbackURL: config.serverURL + '/auth/gitlab/callback'
+      }, passportGeneralCallback))
+
+      gitlabAuth.get('/auth/gitlab', function (req, res, next) {
+        passport.authenticate('gitlab')(req, res, next)
+      })
+
+      // gitlab auth callback
+      gitlabAuth.get('/auth/gitlab/callback',
+        passport.authenticate('gitlab', {
+          successReturnToOrRedirect: config.serverURL + '/',
+          failureRedirect: config.serverURL + '/'
+        })
+      )
+
+      if (!config.gitlab.scope || config.gitlab.scope === 'api'
+      ) {
+        // gitlab callback actions
+        gitlabAuth.get('/auth/gitlab/callback/:noteId/:action', response.gitlabActions)
+      }
+      return gitlabAuth
+    }
+  }
