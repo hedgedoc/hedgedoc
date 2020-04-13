287148f1e85dbae430ab164bb4bc874fa9e2d159
David Mehren <dmehren1@gmail.com>
2020-04-13 15:23:11 +0200

    auth/google: Fix weird type error :label: :bug:
    Signed-off-by: David Mehren <dmehren1@gmail.com>

--- lib/web/auth/google/index.ts
+++ lib/web/auth/google/index.ts
@@ -1,20 +1,33 @@
 import { Router } from 'express'
-import { AuthMiddleware } from '../interface'
 import passport from 'passport'
 import * as Google from 'passport-google-oauth20'
-import {config} from '../../../config'
+import { config } from '../../../config'
+import { AuthMiddleware } from '../interface'
 import { passportGeneralCallback } from '../utils'
 
 const googleAuth = Router()
 
 export const GoogleMiddleware: AuthMiddleware = {
-  getMiddleware (): Router {
+  getMiddleware: function (): Router {
     passport.use(new Google.Strategy({
       clientID: config.google.clientID,
       clientSecret: config.google.clientSecret,
       callbackURL: config.serverURL + '/auth/google/callback',
       userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
-    }, passportGeneralCallback))
+    }, (
+      accessToken: string,
+      refreshToken: string,
+      profile: any,
+      done) => {
+      /*
+       This ugly hack is neccessary, because the Google Strategy wants a done-callback with an err as Error | null | undefined
+       but the passportGeneralCallback (and every other PassportStrategy) want a done-callback with err as string | Error | undefined
+       Note the absence of null. The lambda converts all `null` to `undefined`.
+      */
+      passportGeneralCallback(accessToken, refreshToken, profile, (err?, user?) => {
+        done(err === null ? undefined : err, user)
+      })
+    }))
 
     googleAuth.get('/auth/google', function (req, res, next) {
       const authOpts = { scope: ['profile'], hostedDomain: config.google.hostedDomain }
