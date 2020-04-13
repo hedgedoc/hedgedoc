5c9bef36d1a23a2d02bd600bdd47896c3ee3c087
Philip Molares <philip.molares@udo.edu>
2020-04-13 13:33:08 +0200

    Migrate auth/ldap.js to TypeScript
    Signed-off-by: Philip Molares <philip.molares@udo.edu>
    Signed-off-by: David Mehren <dmehren1@gmail.com>

--- lib/web/auth/index.ts
+++ lib/web/auth/index.ts
@@ -9,7 +9,7 @@ import { GithubMiddleware } from './github'
 import { GitlabMiddleware } from './gitlab'
 import { DropboxMiddleware } from './dropbox'
 import google from './google'
-import ldap from './ldap'
+import { LdapMiddleware } from './ldap'
 import { SamlMiddleware } from './saml'
 import oauth2 from './oauth2'
 import { EmailMiddleware } from './email'
@@ -49,7 +49,7 @@ if (config.isGitHubEnable) AuthRouter.use(GithubMiddleware.getMiddleware())
 if (config.isGitLabEnable) AuthRouter.use(GitlabMiddleware.getMiddleware())
 if (config.isDropboxEnable) AuthRouter.use(DropboxMiddleware.getMiddleware())
 if (config.isGoogleEnable) AuthRouter.use(google)
-if (config.isLDAPEnable) AuthRouter.use(ldap)
+if (config.isLDAPEnable) AuthRouter.use(LdapMiddleware.getMiddleware())
 if (config.isSAMLEnable) AuthRouter.use(SamlMiddleware.getMiddleware())
 if (config.isOAuth2Enable) AuthRouter.use(oauth2)
 if (config.isEmailEnable) AuthRouter.use(EmailMiddleware.getMiddleware())
