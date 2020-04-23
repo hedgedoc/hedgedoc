efff10b71932e6e9ae7e26c09d27e9b00b75c83d
Erik Michelson <erik@liltv.de>
2020-04-24 01:54:03 +0200

    Add more static files to cache
    Signed-off-by: Erik Michelson <erik@liltv.de>

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -2,10 +2,19 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('static-files').then(cache => {
       return cache.addAll([
+        // cache static assets
         'build/font-pack.css',
+        'build/index-styles-pack.css',
+        'build/index-styles.css',
+        'build/index.css',
         'build/cover-styles-pack.css',
         'build/cover.css',
-        'favicon.png'
+        'favicon.png',
+        // cache mathjax static files
+        'js/mathjax-config-extra.js',
+        'build/MathJax/MathJax.js',
+        'build/MathJax/config/TeX-AMS-MML_HTMLorMML.js',
+        'build/MathJax/config/Safe.js'
       ])
     })
   )
