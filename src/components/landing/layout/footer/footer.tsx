0eb80dbf41df9521c8d8b5bc5e84467d9a60c50e
Philip Molares <philip.molares@udo.edu>
2020-05-15 12:36:07 +0200

    correct footer text color to look like the current version fixes #14
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/components/landing/layout/footer/footer.tsx
+++ src/components/landing/layout/footer/footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {LanguagePicker} from "./language-picker";
 import {PoweredByLinks} from "./powered-by-links";
 import {SocialLink} from "./social-links";
+import "./footer.scss";
 
 export const Footer: React.FC = () => {
     return (
