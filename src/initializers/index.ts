dc40dab9af873841d9d29d3a93ef6319893908a2
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2020-05-24 11:24:24 +0200

    add custom delay
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/initializers/index.ts
+++ src/initializers/index.ts
@@ -2,7 +2,15 @@ import {setUpFontAwesome} from "./fontAwesome";
 import {setUpI18n} from "./i18n";
 import {loadAllConfig} from "./configLoader";
 
+function customDelay() {
+    if (!!window.localStorage.getItem("customDelay")) {
+        return new Promise((resolve => setTimeout(resolve, 5000)));
+    } else {
+        return Promise.resolve()
+    }
+}
+
 export function setUp() {
     setUpFontAwesome();
-    return [setUpI18n(), loadAllConfig()]
+    return [setUpI18n(), loadAllConfig(), customDelay()]
 }
