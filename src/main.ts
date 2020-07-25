9fd67eb1add33ae9e98d92e6549c264c229cf199
David Mehren <git@herrmehren.de>
2020-07-25 20:26:10 +0200

    Fix formatting in main.ts
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
-import { ValidationPipe } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-async function bootstrap () {
-  const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: true, skipMissingProperties: false, transform: true }))
-  await app.listen(3000)
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: true,
+      skipMissingProperties: false,
+      transform: true,
+    }),
+  );
+  await app.listen(3000);
 }
 
-bootstrap()
+bootstrap();
