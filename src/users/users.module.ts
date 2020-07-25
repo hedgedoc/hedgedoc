27126bcde1cc3579278c393d136b2d47f0e391d8
David Mehren <git@herrmehren.de>
2020-07-25 20:06:22 +0200

    Add UsersModule
    This contains the module, a service (which only returns mock data), a model and the UserInfo DTO.
    
    Signed-off-by: David Mehren <git@herrmehren.de>

---
+++ src/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersService],
+  exports: [UsersService],
+})
+export class UsersModule {}
