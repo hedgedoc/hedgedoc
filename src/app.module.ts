6e7d98502dfedc39f24d00e29ab6013e5b2f532a
David Mehren <git@herrmehren.de>
2020-07-25 20:26:00 +0200

    Import new modules into AppModule
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
-import { Module } from '@nestjs/common'
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PublicApiModule } from './api/public/public-api.module';
+import { NotesModule } from './notes/notes.module';
+import { UsersModule } from './users/users.module';
+import { RevisionsModule } from './revisions/revisions.module';
+import { AuthorsModule } from './authors/authors.module';
+import { HistoryModule } from './history/history.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './hedgedoc.sqlite',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    NotesModule,
+    UsersModule,
+    RevisionsModule,
+    AuthorsModule,
+    PublicApiModule,
+    HistoryModule,
+  ],
   controllers: [],
-  providers: []
+  providers: [],
 })
-export class AppModule {
-}
+export class AppModule {}
