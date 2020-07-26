99614f8f3a11b112c6411f7a710f8833ae51d0cc
David Mehren <git@herrmehren.de>
2020-07-26 21:20:49 +0200

    Add media upload route to MediaController
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/api/public/media/media.controller.ts
+++ src/api/public/media/media.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Logger,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('media')
-export class MediaController {}
+export class MediaController {
+  private readonly logger = new Logger(MediaController.name);
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadImage(@UploadedFile() file) {
+    this.logger.debug('Recieved file: ' + file);
+  }
+}
