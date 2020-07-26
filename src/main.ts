f3d1644f9581361e743b7f25e59672cd96a9ed06
David Mehren <git@herrmehren.de>
2020-07-26 16:53:43 +0200

    Enable automatic OpenAPI spec generation.
    NestJS can automatically generate an OpenAPI spec by analyzing controllers and used DTOs.
    This commit enables this feature. The API docs are served under /apidoc.
    
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('HedgeDoc')
+    .setVersion('2.0-dev')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('apidoc', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       forbidUnknownValues: true,
