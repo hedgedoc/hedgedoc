574c7d1dd4530b5b737944cc204e6b6669ebbfcc
David Mehren <git@herrmehren.de>
2020-07-26 17:28:32 +0200

    Log warnings when using hardcoded data.
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { UserInfoDto } from './user-info.dto';
 
 @Injectable()
 export class UsersService {
+  private readonly logger = new Logger(UsersService.name);
+
   getUserInfo(): UserInfoDto {
     //TODO: Use the database
+    this.logger.warn('Using hardcoded data!');
     return {
       displayName: 'foo',
       userName: 'fooUser',
