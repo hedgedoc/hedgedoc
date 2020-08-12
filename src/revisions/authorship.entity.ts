c6816f9bba641325e771918ce48ecd375493da0f
David Mehren <git@herrmehren.de>
2020-08-12 20:51:49 +0200

    Add authorship entity.
    It stores which parts of a revision were edited by a particular user.
    
    Signed-off-by: David Mehren <git@herrmehren.de>

---
+++ src/revisions/authorship.entity.ts
@@ -0,0 +1,45 @@
+import {
+  Column, CreateDateColumn,
+  Entity,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn, UpdateDateColumn,
+} from 'typeorm/index';
+import { User } from '../users/user.entity';
+import { Revision } from './revision.entity';
+
+/**
+ * This class stores which parts of a revision were edited by a particular user.
+ */
+@Entity()
+export class Authorship {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  /**
+   * Revisions this authorship appears in
+   */
+  @ManyToMany(
+    _ => Revision,
+    revision => revision.authorships,
+  )
+  revisions: Revision[];
+
+  /**
+   * User this authorship represents
+   */
+  @ManyToOne(_ => User)
+  user: User;
+
+  @Column()
+  startPos: number
+
+  @Column()
+  endPos: number
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+}
