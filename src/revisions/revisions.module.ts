0a33d8ef8d5b7cfd127f7f81b4fc8b2752e53080
David Mehren <git@herrmehren.de>
2020-08-12 20:51:49 +0200

    Add authorship entity.
    It stores which parts of a revision were edited by a particular user.
    
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/revisions/revisions.module.ts
+++ src/revisions/revisions.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Authorship } from './authorship.entity';
 import { Revision } from './revision.entity';
 import { RevisionsService } from './revisions.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Revision])],
+  imports: [TypeOrmModule.forFeature([Revision, Authorship])],
   providers: [RevisionsService],
   exports: [RevisionsService],
 })
