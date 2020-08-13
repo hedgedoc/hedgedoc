669dd01ecce4407a911f7c0b2129705bb82926e3
David Mehren <git@herrmehren.de>
2020-08-13 20:22:04 +0200

    Initialize TypeORM for Group entity
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Group } from './group.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Group])],
+})
 export class GroupsModule {}
