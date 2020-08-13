8689b44f597a17e07428f05edefa43bb93e3e123
David Mehren <git@herrmehren.de>
2020-08-13 20:26:47 +0200

    Implement User entity.
    This commit implements the User entity according to the database schema and adds the Identity and AuthToken entities.
    
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthToken } from './auth-token.entity';
+import { Identity } from './identity.entity';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, AuthToken, Identity])],
   providers: [UsersService],
   exports: [UsersService],
 })
