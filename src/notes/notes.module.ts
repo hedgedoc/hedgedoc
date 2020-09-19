9cae7e34dc29243e32ecc486e50edeab16ff08cb
David Mehren <git@herrmehren.de>
2020-09-19 17:40:50 +0200

    NotesService: Get more note metadata from the database
    Some previously hardcoded metadata-values are now retrieved from the database.
    
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RevisionsModule } from '../revisions/revisions.module';
+import { UsersModule } from '../users/users.module';
 import { AuthorColor } from './author-color.entity';
 import { Note } from './note.entity';
 import { NotesService } from './notes.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Note, AuthorColor])],
+  imports: [
+    TypeOrmModule.forFeature([Note, AuthorColor]),
+    RevisionsModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [NotesService],
   exports: [NotesService],
