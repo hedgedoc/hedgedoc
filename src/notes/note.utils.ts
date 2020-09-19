f9370424392caa46d31848b50f670f2ae0928a96
David Mehren <git@herrmehren.de>
2020-09-19 17:32:08 +0200

    NoteUtils: Add methods to parse note metadata
    These methods are intended to parse metadata details from YAML tags, but not implemented for now.
    
    Signed-off-by: David Mehren <git@herrmehren.de>

---
+++ src/notes/note.utils.ts
@@ -0,0 +1,18 @@
+import { Note } from './note.entity';
+
+export class NoteUtils {
+  public static parseTitle(note: Note): string {
+    // TODO: Implement method
+    return 'Hardcoded note title';
+  }
+
+  public static parseDescription(note: Note): string {
+    // TODO: Implement method
+    return 'Hardcoded note description';
+  }
+
+  public static parseTags(note: Note): string[] {
+    // TODO: Implement method
+    return ['Hardcoded note tag'];
+  }
+}
