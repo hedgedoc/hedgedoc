b9b5af8b7ff053f8776ee52c6e0a6845c9a4cd87
David Mehren <git@herrmehren.de>
2020-09-22 21:59:09 +0200

    Add various missing imports and provider overrides to fix unit tests.
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/revisions/revisions.service.spec.ts
+++ src/revisions/revisions.service.spec.ts
@@ -1,4 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { AuthorColor } from '../notes/author-color.entity';
+import { Note } from '../notes/note.entity';
+import { NotesModule } from '../notes/notes.module';
+import { AuthToken } from '../users/auth-token.entity';
+import { Identity } from '../users/identity.entity';
+import { User } from '../users/user.entity';
+import { Authorship } from './authorship.entity';
+import { Revision } from './revision.entity';
 import { RevisionsService } from './revisions.service';
 
 describe('RevisionsService', () => {
@@ -6,8 +15,30 @@ describe('RevisionsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RevisionsService],
-    }).compile();
+      providers: [
+        RevisionsService,
+        {
+          provide: getRepositoryToken(Revision),
+          useValue: {},
+        },
+      ],
+      imports: [NotesModule],
+    })
+      .overrideProvider(getRepositoryToken(Authorship))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(AuthorColor))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(User))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(AuthToken))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(Identity))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(Note))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(Revision))
+      .useValue({})
+      .compile();
 
     service = module.get<RevisionsService>(RevisionsService);
   });
