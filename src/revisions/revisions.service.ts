74b03fc1fd5d4b6c5b4cc48308223ca2c7c3dee6
David Mehren <git@herrmehren.de>
2020-09-22 17:29:59 +0200

    RevisionsService: Implement `getLatestRevision` and `createRevision` methods
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/revisions/revisions.service.ts
+++ src/revisions/revisions.service.ts
@@ -1,9 +1,16 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { RevisionMetadataDto } from './revision-metadata.dto';
 import { RevisionDto } from './revision.dto';
+import { Revision } from './revision.entity';
 
 @Injectable()
 export class RevisionsService {
+  constructor(
+    @InjectRepository(Revision)
+    private revisionRepository: Repository<Revision>,
+  ) {}
   private readonly logger = new Logger(RevisionsService.name);
   getNoteRevisionMetadatas(noteIdOrAlias: string): RevisionMetadataDto[] {
     this.logger.warn('Using hardcoded data!');
@@ -23,5 +30,26 @@ export class RevisionsService {
       content: 'Foobar',
       patch: 'barfoo',
     };
+  }
+
+  getLatestRevision(noteId: string): Promise<Revision> {
+    return this.revisionRepository.findOne({
+      where: {
+        note: noteId,
+      },
+      order: {
+        createdAt: 'DESC',
+      },
+    });
+  }
+
+  createRevision(content: string) {
+    // TODO: Add previous revision
+    // TODO: Calculate patch
+    return this.revisionRepository.create({
+      content: content,
+      length: content.length,
+      patch: '',
+    });
   }
 }
