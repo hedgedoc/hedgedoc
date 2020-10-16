e2b5acaf840c22e04bd298a2054be3dd10a8d1c7
David Mehren <git@herrmehren.de>
2020-10-16 22:37:20 +0200

    MediaController: Use `MediaService` to store media
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/api/public/media/media.controller.ts
+++ src/api/public/media/media.controller.ts
@@ -6,16 +6,30 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ConsoleLoggerService } from '../../../logger/console-logger.service';
+import { MediaService } from '../../../media/media.service';
+import { MulterFile } from '../../../media/multer-file.interface';
 
 @Controller('media')
 export class MediaController {
-  constructor(private readonly logger: ConsoleLoggerService) {
+  constructor(
+    private readonly logger: ConsoleLoggerService,
+    private mediaService: MediaService,
+  ) {
     this.logger.setContext(MediaController.name);
   }
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  uploadImage(@UploadedFile() file) {
-    this.logger.debug('Recieved file: ' + file);
+  async uploadImage(@UploadedFile() file: MulterFile) {
+    this.logger.debug('Recieved file: ' + file.originalname);
+    //TODO: Get user and note from request
+    const url = await this.mediaService.saveFile(
+      file,
+      'hardcoded',
+      'hardcoded',
+    );
+    return {
+      link: url,
+    };
   }
 }
