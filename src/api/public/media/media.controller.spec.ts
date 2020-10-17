ed142815e3b5ccecf7631bc7d5ad407bf78f7ba2
David Mehren <git@herrmehren.de>
2020-10-17 20:21:22 +0200

    Add various missing imports and provider ovverides in unit tests
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/api/public/media/media.controller.spec.ts
+++ src/api/public/media/media.controller.spec.ts
@@ -1,5 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { LoggerModule } from '../../../logger/logger.module';
+import { MediaUpload } from '../../../media/media-upload.entity';
+import { MediaModule } from '../../../media/media.module';
+import { AuthorColor } from '../../../notes/author-color.entity';
+import { Note } from '../../../notes/note.entity';
+import { NotesModule } from '../../../notes/notes.module';
+import { Authorship } from '../../../revisions/authorship.entity';
+import { Revision } from '../../../revisions/revision.entity';
+import { AuthToken } from '../../../users/auth-token.entity';
+import { Identity } from '../../../users/identity.entity';
+import { User } from '../../../users/user.entity';
 import { MediaController } from './media.controller';
 
 describe('Media Controller', () => {
@@ -8,8 +19,25 @@ describe('Media Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MediaController],
-      imports: [LoggerModule],
-    }).compile();
+      imports: [LoggerModule, MediaModule, NotesModule],
+    })
+      .overrideProvider(getRepositoryToken(AuthorColor))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(Authorship))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(AuthToken))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(Identity))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(MediaUpload))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(Note))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(Revision))
+      .useValue({})
+      .overrideProvider(getRepositoryToken(User))
+      .useValue({})
+      .compile();
 
     controller = module.get<MediaController>(MediaController);
   });
