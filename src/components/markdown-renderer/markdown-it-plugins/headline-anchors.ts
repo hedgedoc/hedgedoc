005597e8805438bc6bda74530955318c503bf9ba
Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>
2021-01-07 17:52:00 +0100

    Use original markdown-it-anchor (#914)
    Signed-off-by: Tilman Vatteroth <tilman.vatteroth@tu-dortmund.de>

--- src/components/markdown-renderer/markdown-it-plugins/headline-anchors.ts
+++ src/components/markdown-renderer/markdown-it-plugins/headline-anchors.ts
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import anchor from '@mrdrogdrog/markdown-it-anchor'
 import MarkdownIt from 'markdown-it'
+import anchor from 'markdown-it-anchor'
 
 export const headlineAnchors: MarkdownIt.PluginSimple = (markdownIt) => {
   const options: anchor.AnchorOptions = {
@@ -13,7 +13,7 @@ export const headlineAnchors: MarkdownIt.PluginSimple = (markdownIt) => {
     permalinkBefore: true,
     permalinkClass: 'heading-anchor text-dark',
     permalinkSymbol: '<i class="fa fa-link"></i>',
-    permalinkHref: (slug: string): string => slug
+    permalinkHref: (slug: string): string => `#${slug}`
   }
 
   anchor(markdownIt, options)
