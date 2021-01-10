371367d1e46ce43ecf30c0edeeeff8c94d70ab1e
Philip Molares <philip.molares@udo.edu>
2021-01-10 22:39:05 +0100

    implemented the new way of getting multiple auth provider via env variables.
    First you specify in a separate env var how your instances of gitlab, ldap, saml or oauth2 are called in the config. Then you put those names into the env variable names to get unique
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/config/auth-config.ts
+++ src/config/auth-config.ts
@@ -207,6 +207,91 @@ export const authSchema = Joi.object({
     .optional(),
 });
 
+// ToDo: Validate these with Joi to prevent duplicate entries?
+
+const gitlabNames = toArrayConfig(process.env.HD_AUTH_GITLABS, ',');
+const ldapNames = toArrayConfig(process.env.HD_AUTH_LDAPS, ',');
+const samlNames = toArrayConfig(process.env.HD_AUTH_SAMLS, ',');
+const oauth2Names = toArrayConfig(process.env.HD_AUTH_OAUTH2S, ',');
+
+const gitlabs = gitlabNames.map(gitlabName => {
+  return {
+    providerName: process.env[`HD_AUTH_GITLAB_${gitlabName}_PROVIDER_NAME`],
+    baseURL: process.env[`HD_AUTH_GITLAB_${gitlabName}_BASE_URL`],
+    clientID: process.env[`HD_AUTH_GITLAB_${gitlabName}_CLIENT_ID`],
+    clientSecret: process.env[`HD_AUTH_GITLAB_${gitlabName}_CLIENT_SECRET`],
+    scope: process.env[`HD_AUTH_GITLAB_${gitlabName}_GITLAB_SCOPE`],
+    version: process.env[`HD_AUTH_GITLAB_${gitlabName}_GITLAB_VERSION`],
+  };
+});
+
+const ldaps = ldapNames.map(ldapName => {
+  return {
+    providerName: process.env[`HD_AUTH_LDAP_${ldapName}_PROVIDER_NAME`],
+    url: process.env[`HD_AUTH_LDAP_${ldapName}_URL`],
+    bindDn: process.env[`HD_AUTH_LDAP_${ldapName}_BIND_DN`],
+    bindCredentials: process.env[`HD_AUTH_LDAP_${ldapName}_BIND_CREDENTIALS`],
+    searchBase: process.env[`HD_AUTH_LDAP_${ldapName}_SEARCH_BASE`],
+    searchFilter: process.env[`HD_AUTH_LDAP_${ldapName}_SEARCH_FILTER`],
+    searchAttributes: toArrayConfig(
+      process.env[`HD_AUTH_LDAP_${ldapName}_SEARCH_ATTRIBUTES`],
+      ',',
+    ),
+    usernameField: process.env[`HD_AUTH_LDAP_${ldapName}_USERNAME_FIELD`],
+    useridField: process.env[`HD_AUTH_LDAP_${ldapName}_USERID_FIELD`],
+    tlsCa: toArrayConfig(process.env[`HD_AUTH_LDAP_${ldapName}_TLS_CA`], ','),
+  };
+});
+
+const samls = samlNames.map(samlName => {
+  return {
+    providerName: process.env[`HD_AUTH_SAML_${samlName}_PROVIDER_NAME`],
+    idpSsoUrl: process.env[`HD_AUTH_SAML_${samlName}_IDPSSOURL`],
+    idpCert: process.env[`HD_AUTH_SAML_${samlName}_IDPCERT`],
+    clientCert: process.env[`HD_AUTH_SAML_${samlName}_CLIENTCERT`],
+    issuer: process.env[`HD_AUTH_SAML_${samlName}_ISSUER`],
+    identifierFormat: process.env[`HD_AUTH_SAML_${samlName}_IDENTIFIERFORMAT`],
+    disableRequestedAuthnContext: process.env[`HD_AUTH_SAML_${samlName}_DISABLEREQUESTEDAUTHNCONTEXT`],
+    groupAttribute: process.env[`HD_AUTH_SAML_${samlName}_GROUPATTRIBUTE`],
+    requiredGroups: toArrayConfig(
+      process.env[`HD_AUTH_SAML_${samlName}_REQUIREDGROUPS`],
+      '|',
+    ),
+    externalGroups: toArrayConfig(
+      process.env[`HD_AUTH_SAML_${samlName}_EXTERNALGROUPS`],
+      '|',
+    ),
+    attribute: {
+      id: process.env[`HD_AUTH_SAML_${samlName}_ATTRIBUTE_ID`],
+      username: process.env[`HD_AUTH_SAML_${samlName}_ATTRIBUTE_USERNAME`],
+      email: process.env[`HD_AUTH_SAML_${samlName}_ATTRIBUTE_USERNAME`],
+    },
+  };
+});
+
+const oauth2s = oauth2Names.map(oauth2Name => {
+  return {
+    providerName: process.env[`HD_AUTH_OAUTH2_${oauth2Name}_PROVIDER_NAME`],
+    baseURL: process.env[`HD_AUTH_OAUTH2_${oauth2Name}_BASEURL`],
+    userProfileURL: process.env[`HD_AUTH_OAUTH2_${oauth2Name}_USER_PROFILE_URL`],
+    userProfileIdAttr:
+      process.env[`HD_AUTH_OAUTH2_${oauth2Name}_USER_PROFILE_ID_ATTR`],
+    userProfileUsernameAttr:
+      process.env[`HD_AUTH_OAUTH2_${oauth2Name}_USER_PROFILE_USERNAME_ATTR`],
+    userProfileDisplayNameAttr:
+      process.env[`HD_AUTH_OAUTH2_${oauth2Name}_USER_PROFILE_DISPLAY_NAME_ATTR`],
+    userProfileEmailAttr:
+      process.env[`HD_AUTH_OAUTH2_${oauth2Name}_USER_PROFILE_EMAIL_ATTR`],
+    tokenURL: process.env[`HD_AUTH_OAUTH2_${oauth2Name}_TOKEN_URL`],
+    authorizationURL: process.env[`HD_AUTH_OAUTH2_${oauth2Name}_AUTHORIZATION_URL`],
+    clientID: process.env[`HD_AUTH_OAUTH2_${oauth2Name}_CLIENT_ID`],
+    clientSecret: process.env[`HD_AUTH_OAUTH2_${oauth2Name}_CLIENT_SECRET`],
+    scope: process.env[`HD_AUTH_OAUTH2_${oauth2Name}_SCOPE`],
+    rolesClaim: process.env[`HD_AUTH_OAUTH2_${oauth2Name}_ROLES_CLAIM`],
+    accessRole: process.env[`HD_AUTH_OAUTH2_${oauth2Name}_ACCESS_ROLE`],
+  };
+});
+
 export const appConfigAuth = {
   email: {
     enableLogin: process.env.HD_AUTH_EMAIL_ENABLE_LOGIN,
@@ -234,347 +319,8 @@ export const appConfigAuth = {
     clientSecret: process.env.HD_AUTH_GOOGLE_CLIENT_SECRET,
     apiKey: process.env.HD_AUTH_GOOGLE_APP_KEY,
   },
-  gitlab: [
-    {
-      providerName: process.env.HD_AUTH_GITLAB_PROVIDER_NAME_0,
-      baseURL: process.env.HD_AUTH_GITLAB_BASE_URL_0,
-      clientID: process.env.HD_AUTH_GITLAB_CLIENT_ID_0,
-      clientSecret: process.env.HD_AUTH_GITLAB_CLIENT_SECRET_0,
-      scope: process.env.HD_AUTH_GITLAB_SCOPE_0,
-      version: process.env.HD_AUTH_GITLAB_VERSION_0,
-    },
-    {
-      providerName: process.env.HD_AUTH_GITLAB_PROVIDER_NAME_1,
-      baseURL: process.env.HD_AUTH_GITLAB_BASE_URL_1,
-      clientID: process.env.HD_AUTH_GITLAB_CLIENT_ID_1,
-      clientSecret: process.env.HD_AUTH_GITLAB_CLIENT_SECRET_1,
-      scope: process.env.HD_AUTH_GITLAB_SCOPE_1,
-      version: process.env.HD_AUTH_GITLAB_VERSION_1,
-    },
-    {
-      providerName: process.env.HD_AUTH_GITLAB_PROVIDER_NAME_2,
-      baseURL: process.env.HD_AUTH_GITLAB_BASE_URL_2,
-      clientID: process.env.HD_AUTH_GITLAB_CLIENT_ID_2,
-      clientSecret: process.env.HD_AUTH_GITLAB_CLIENT_SECRET_2,
-      scope: process.env.HD_AUTH_GITLAB_SCOPE_2,
-      version: process.env.HD_AUTH_GITLAB_VERSION_2,
-    },
-    {
-      providerName: process.env.HD_AUTH_GITLAB_PROVIDER_NAME_3,
-      baseURL: process.env.HD_AUTH_GITLAB_BASE_URL_3,
-      clientID: process.env.HD_AUTH_GITLAB_CLIENT_ID_3,
-      clientSecret: process.env.HD_AUTH_GITLAB_CLIENT_SECRET_3,
-      scope: process.env.HD_AUTH_GITLAB_SCOPE_3,
-      version: process.env.HD_AUTH_GITLAB_VERSION_3,
-    },
-    {
-      providerName: process.env.HD_AUTH_GITLAB_PROVIDER_NAME_4,
-      baseURL: process.env.HD_AUTH_GITLAB_BASE_URL_4,
-      clientID: process.env.HD_AUTH_GITLAB_CLIENT_ID_4,
-      clientSecret: process.env.HD_AUTH_GITLAB_CLIENT_SECRET_4,
-      scope: process.env.HD_AUTH_GITLAB_SCOPE_4,
-      version: process.env.HD_AUTH_GITLAB_VERSION_4,
-    },
-  ],
-  ldap: [
-    {
-      providerName: process.env.HD_AUTH_LDAP_PROVIDER_NAME_0,
-      url: process.env.HD_AUTH_LDAP_URL_0,
-      bindDn: process.env.HD_AUTH_LDAP_BIND_DN_0,
-      bindCredentials: process.env.HD_AUTH_LDAP_BIND_CREDENTIALS_0,
-      searchBase: process.env.HD_AUTH_LDAP_SEARCH_BASE_0,
-      searchFilter: process.env.HD_AUTH_LDAP_SEARCH_FILTER_0,
-      searchAttributes: toArrayConfig(
-        process.env.HD_AUTH_LDAP_SEARCH_ATTRIBUTES_0,
-        ',',
-      ),
-      usernameField: process.env.HD_AUTH_LDAP_USERNAME_FIELD_0,
-      useridField: process.env.HD_AUTH_LDAP_USERID_FIELD_0,
-      tlsCa: toArrayConfig(process.env.HD_AUTH_LDAP_TLS_CA_0, ','),
-    },
-    {
-      providerName: process.env.HD_AUTH_LDAP_PROVIDER_NAME_1,
-      url: process.env.HD_AUTH_LDAP_URL_1,
-      bindDn: process.env.HD_AUTH_LDAP_BIND_DN_1,
-      bindCredentials: process.env.HD_AUTH_LDAP_BIND_CREDENTIALS_1,
-      searchBase: process.env.HD_AUTH_LDAP_SEARCH_BASE_1,
-      searchFilter: process.env.HD_AUTH_LDAP_SEARCH_FILTER_1,
-      searchAttributes: toArrayConfig(
-        process.env.HD_AUTH_LDAP_SEARCH_ATTRIBUTES_1,
-        ',',
-      ),
-      usernameField: process.env.HD_AUTH_LDAP_USERNAME_FIELD_1,
-      useridField: process.env.HD_AUTH_LDAP_USERID_FIELD_1,
-      tlsCa: toArrayConfig(process.env.HD_AUTH_LDAP_TLS_CA_1, ','),
-    },
-    {
-      providerName: process.env.HD_AUTH_LDAP_PROVIDER_NAME_2,
-      url: process.env.HD_AUTH_LDAP_URL_2,
-      bindDn: process.env.HD_AUTH_LDAP_BIND_DN_2,
-      bindCredentials: process.env.HD_AUTH_LDAP_BIND_CREDENTIALS_2,
-      searchBase: process.env.HD_AUTH_LDAP_SEARCH_BASE_2,
-      searchFilter: process.env.HD_AUTH_LDAP_SEARCH_FILTER_2,
-      searchAttributes: toArrayConfig(
-        process.env.HD_AUTH_LDAP_SEARCH_ATTRIBUTES_2,
-        ',',
-      ),
-      usernameField: process.env.HD_AUTH_LDAP_USERNAME_FIELD_2,
-      useridField: process.env.HD_AUTH_LDAP_USERID_FIELD_2,
-      tlsCa: toArrayConfig(process.env.HD_AUTH_LDAP_TLS_CA_2, ','),
-    },
-    {
-      providerName: process.env.HD_AUTH_LDAP_PROVIDER_NAME_3,
-      url: process.env.HD_AUTH_LDAP_URL_3,
-      bindDn: process.env.HD_AUTH_LDAP_BIND_DN_3,
-      bindCredentials: process.env.HD_AUTH_LDAP_BIND_CREDENTIALS_3,
-      searchBase: process.env.HD_AUTH_LDAP_SEARCH_BASE_3,
-      searchFilter: process.env.HD_AUTH_LDAP_SEARCH_FILTER_3,
-      searchAttributes: toArrayConfig(
-        process.env.HD_AUTH_LDAP_SEARCH_ATTRIBUTES_3,
-        ',',
-      ),
-      usernameField: process.env.HD_AUTH_LDAP_USERNAME_FIELD_3,
-      useridField: process.env.HD_AUTH_LDAP_USERID_FIELD_3,
-      tlsCa: toArrayConfig(process.env.HD_AUTH_LDAP_TLS_CA_3, ','),
-    },
-    {
-      providerName: process.env.HD_AUTH_LDAP_PROVIDER_NAME_4,
-      url: process.env.HD_AUTH_LDAP_URL_4,
-      bindDn: process.env.HD_AUTH_LDAP_BIND_DN_4,
-      bindCredentials: process.env.HD_AUTH_LDAP_BIND_CREDENTIALS_4,
-      searchBase: process.env.HD_AUTH_LDAP_SEARCH_BASE_4,
-      searchFilter: process.env.HD_AUTH_LDAP_SEARCH_FILTER_4,
-      searchAttributes: toArrayConfig(
-        process.env.HD_AUTH_LDAP_SEARCH_ATTRIBUTES_4,
-        ',',
-      ),
-      usernameField: process.env.HD_AUTH_LDAP_USERNAME_FIELD_4,
-      useridField: process.env.HD_AUTH_LDAP_USERID_FIELD_4,
-      tlsCa: toArrayConfig(process.env.HD_AUTH_LDAP_TLS_CA_4, ','),
-    },
-  ],
-  saml: [
-    {
-      providerName: process.env.HD_AUTH_SAML_PROVIDER_NAME_0,
-      idpSsoUrl: process.env.HD_AUTH_SAML_IDPSSOURL_0,
-      idpCert: process.env.HD_AUTH_SAML_IDPCERT_0,
-      clientCert: process.env.HD_AUTH_SAML_CLIENTCERT_0,
-      issuer: process.env.HD_AUTH_SAML_ISSUER_0,
-      identifierFormat: process.env.HD_AUTH_SAML_IDENTIFIERFORMAT_0,
-      disableRequestedAuthnContext:
-      process.env.HD_AUTH_SAML_DISABLEREQUESTEDAUTHNCONTEXT_0,
-      groupAttribute: process.env.HD_AUTH_SAML_GROUPATTRIBUTE_0,
-      requiredGroups: toArrayConfig(
-        process.env.HD_AUTH_SAML_REQUIREDGROUPS_0,
-        '|',
-      ),
-      externalGroups: toArrayConfig(
-        process.env.HD_AUTH_SAML_EXTERNALGROUPS_0,
-        '|',
-      ),
-      attribute: {
-        id: process.env.HD_AUTH_SAML_ATTRIBUTE_ID_0,
-        username: process.env.HD_AUTH_SAML_ATTRIBUTE_USERNAME_0,
-        email: process.env.HD_AUTH_SAML_ATTRIBUTE_USERNAME_0,
-      },
-    },
-    {
-      providerName: process.env.HD_AUTH_SAML_PROVIDER_NAME_1,
-      idpSsoUrl: process.env.HD_AUTH_SAML_IDPSSOURL_1,
-      idpCert: process.env.HD_AUTH_SAML_IDPCERT_1,
-      clientCert: process.env.HD_AUTH_SAML_CLIENTCERT_1,
-      issuer: process.env.HD_AUTH_SAML_ISSUER_1,
-      identifierFormat: process.env.HD_AUTH_SAML_IDENTIFIERFORMAT_1,
-      disableRequestedAuthnContext:
-      process.env.HD_AUTH_SAML_DISABLEREQUESTEDAUTHNCONTEXT_1,
-      groupAttribute: process.env.HD_AUTH_SAML_GROUPATTRIBUTE_1,
-      requiredGroups: toArrayConfig(
-        process.env.HD_AUTH_SAML_REQUIREDGROUPS_1,
-        '|',
-      ),
-      externalGroups: toArrayConfig(
-        process.env.HD_AUTH_SAML_EXTERNALGROUPS_1,
-        '|',
-      ),
-      attribute: {
-        id: process.env.HD_AUTH_SAML_ATTRIBUTE_ID_1,
-        username: process.env.HD_AUTH_SAML_ATTRIBUTE_USERNAME_1,
-        email: process.env.HD_AUTH_SAML_ATTRIBUTE_USERNAME_1,
-      },
-    },
-    {
-      providerName: process.env.HD_AUTH_SAML_PROVIDER_NAME_2,
-      idpSsoUrl: process.env.HD_AUTH_SAML_IDPSSOURL_2,
-      idpCert: process.env.HD_AUTH_SAML_IDPCERT_2,
-      clientCert: process.env.HD_AUTH_SAML_CLIENTCERT_2,
-      issuer: process.env.HD_AUTH_SAML_ISSUER_2,
-      identifierFormat: process.env.HD_AUTH_SAML_IDENTIFIERFORMAT_2,
-      disableRequestedAuthnContext:
-      process.env.HD_AUTH_SAML_DISABLEREQUESTEDAUTHNCONTEXT_2,
-      groupAttribute: process.env.HD_AUTH_SAML_GROUPATTRIBUTE_2,
-      requiredGroups: toArrayConfig(
-        process.env.HD_AUTH_SAML_REQUIREDGROUPS_2,
-        '|',
-      ),
-      externalGroups: toArrayConfig(
-        process.env.HD_AUTH_SAML_EXTERNALGROUPS_2,
-        '|',
-      ),
-      attribute: {
-        id: process.env.HD_AUTH_SAML_ATTRIBUTE_ID_2,
-        username: process.env.HD_AUTH_SAML_ATTRIBUTE_USERNAME_2,
-        email: process.env.HD_AUTH_SAML_ATTRIBUTE_USERNAME_2,
-      },
-    },
-    {
-      providerName: process.env.HD_AUTH_SAML_PROVIDER_NAME_3,
-      idpSsoUrl: process.env.HD_AUTH_SAML_IDPSSOURL_3,
-      idpCert: process.env.HD_AUTH_SAML_IDPCERT_3,
-      clientCert: process.env.HD_AUTH_SAML_CLIENTCERT_3,
-      issuer: process.env.HD_AUTH_SAML_ISSUER_3,
-      identifierFormat: process.env.HD_AUTH_SAML_IDENTIFIERFORMAT_3,
-      disableRequestedAuthnContext:
-      process.env.HD_AUTH_SAML_DISABLEREQUESTEDAUTHNCONTEXT_3,
-      groupAttribute: process.env.HD_AUTH_SAML_GROUPATTRIBUTE_3,
-      requiredGroups: toArrayConfig(
-        process.env.HD_AUTH_SAML_REQUIREDGROUPS_3,
-        '|',
-      ),
-      externalGroups: toArrayConfig(
-        process.env.HD_AUTH_SAML_EXTERNALGROUPS_3,
-        '|',
-      ),
-      attribute: {
-        id: process.env.HD_AUTH_SAML_ATTRIBUTE_ID_3,
-        username: process.env.HD_AUTH_SAML_ATTRIBUTE_USERNAME_3,
-        email: process.env.HD_AUTH_SAML_ATTRIBUTE_USERNAME_3,
-      },
-    },
-    {
-      providerName: process.env.HD_AUTH_SAML_PROVIDER_NAME_4,
-      idpSsoUrl: process.env.HD_AUTH_SAML_IDPSSOURL_4,
-      idpCert: process.env.HD_AUTH_SAML_IDPCERT_4,
-      clientCert: process.env.HD_AUTH_SAML_CLIENTCERT_4,
-      issuer: process.env.HD_AUTH_SAML_ISSUER_4,
-      identifierFormat: process.env.HD_AUTH_SAML_IDENTIFIERFORMAT_4,
-      disableRequestedAuthnContext:
-      process.env.HD_AUTH_SAML_DISABLEREQUESTEDAUTHNCONTEXT_4,
-      groupAttribute: process.env.HD_AUTH_SAML_GROUPATTRIBUTE_4,
-      requiredGroups: toArrayConfig(
-        process.env.HD_AUTH_SAML_REQUIREDGROUPS_4,
-        '|',
-      ),
-      externalGroups: toArrayConfig(
-        process.env.HD_AUTH_SAML_EXTERNALGROUPS_4,
-        '|',
-      ),
-      attribute: {
-        id: process.env.HD_AUTH_SAML_ATTRIBUTE_ID_4,
-        username: process.env.HD_AUTH_SAML_ATTRIBUTE_USERNAME_4,
-        email: process.env.HD_AUTH_SAML_ATTRIBUTE_USERNAME_4,
-      },
-    },
-  ],
-  oauth2: [
-    {
-      providerName: process.env.HD_AUTH_OAUTH2_PROVIDERNAME_0,
-      baseURL: process.env.HD_AUTH_OAUTH2_BASEURL_0,
-      userProfileURL: process.env.HD_AUTH_OAUTH2_USER_PROFILE_URL_0,
-      userProfileIdAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_ID_ATTR_0,
-      userProfileUsernameAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_USERNAME_ATTR_0,
-      userProfileDisplayNameAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_DISPLAY_NAME_ATTR_0,
-      userProfileEmailAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_EMAIL_ATTR_0,
-      tokenURL: process.env.HD_AUTH_OAUTH2_TOKEN_URL_0,
-      authorizationURL: process.env.HD_AUTH_OAUTH2_AUTHORIZATION_URL_0,
-      clientID: process.env.HD_AUTH_OAUTH2_CLIENT_ID_0,
-      clientSecret: process.env.HD_AUTH_OAUTH2_CLIENT_SECRET_0,
-      scope: process.env.HD_AUTH_OAUTH2_SCOPE_0,
-      rolesClaim: process.env.HD_AUTH_OAUTH2_ROLES_CLAIM_0,
-      accessRole: process.env.HD_AUTH_OAUTH2_ACCESS_ROLE_0,
-    },
-    {
-      providerName: process.env.HD_AUTH_OAUTH2_PROVIDERNAME_1,
-      baseURL: process.env.HD_AUTH_OAUTH2_BASEURL_1,
-      userProfileURL: process.env.HD_AUTH_OAUTH2_USER_PROFILE_URL_1,
-      userProfileIdAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_ID_ATTR_1,
-      userProfileUsernameAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_USERNAME_ATTR_1,
-      userProfileDisplayNameAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_DISPLAY_NAME_ATTR_1,
-      userProfileEmailAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_EMAIL_ATTR_1,
-      tokenURL: process.env.HD_AUTH_OAUTH2_TOKEN_URL_1,
-      authorizationURL: process.env.HD_AUTH_OAUTH2_AUTHORIZATION_URL_1,
-      clientID: process.env.HD_AUTH_OAUTH2_CLIENT_ID_1,
-      clientSecret: process.env.HD_AUTH_OAUTH2_CLIENT_SECRET_1,
-      scope: process.env.HD_AUTH_OAUTH2_SCOPE_1,
-      rolesClaim: process.env.HD_AUTH_OAUTH2_ROLES_CLAIM_1,
-      accessRole: process.env.HD_AUTH_OAUTH2_ACCESS_ROLE_1,
-    },
-    {
-      providerName: process.env.HD_AUTH_OAUTH2_PROVIDERNAME_2,
-      baseURL: process.env.HD_AUTH_OAUTH2_BASEURL_2,
-      userProfileURL: process.env.HD_AUTH_OAUTH2_USER_PROFILE_URL_2,
-      userProfileIdAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_ID_ATTR_2,
-      userProfileUsernameAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_USERNAME_ATTR_2,
-      userProfileDisplayNameAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_DISPLAY_NAME_ATTR_2,
-      userProfileEmailAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_EMAIL_ATTR_2,
-      tokenURL: process.env.HD_AUTH_OAUTH2_TOKEN_URL_2,
-      authorizationURL: process.env.HD_AUTH_OAUTH2_AUTHORIZATION_URL_2,
-      clientID: process.env.HD_AUTH_OAUTH2_CLIENT_ID_2,
-      clientSecret: process.env.HD_AUTH_OAUTH2_CLIENT_SECRET_2,
-      scope: process.env.HD_AUTH_OAUTH2_SCOPE_2,
-      rolesClaim: process.env.HD_AUTH_OAUTH2_ROLES_CLAIM_2,
-      accessRole: process.env.HD_AUTH_OAUTH2_ACCESS_ROLE_2,
-    },
-    {
-      providerName: process.env.HD_AUTH_OAUTH2_PROVIDERNAME_3,
-      baseURL: process.env.HD_AUTH_OAUTH2_BASEURL_3,
-      userProfileURL: process.env.HD_AUTH_OAUTH2_USER_PROFILE_URL_3,
-      userProfileIdAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_ID_ATTR_3,
-      userProfileUsernameAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_USERNAME_ATTR_3,
-      userProfileDisplayNameAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_DISPLAY_NAME_ATTR_3,
-      userProfileEmailAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_EMAIL_ATTR_3,
-      tokenURL: process.env.HD_AUTH_OAUTH2_TOKEN_URL_3,
-      authorizationURL: process.env.HD_AUTH_OAUTH2_AUTHORIZATION_URL_3,
-      clientID: process.env.HD_AUTH_OAUTH2_CLIENT_ID_3,
-      clientSecret: process.env.HD_AUTH_OAUTH2_CLIENT_SECRET_3,
-      scope: process.env.HD_AUTH_OAUTH2_SCOPE_3,
-      rolesClaim: process.env.HD_AUTH_OAUTH2_ROLES_CLAIM_3,
-      accessRole: process.env.HD_AUTH_OAUTH2_ACCESS_ROLE_3,
-    },
-    {
-      providerName: process.env.HD_AUTH_OAUTH2_PROVIDERNAME_4,
-      baseURL: process.env.HD_AUTH_OAUTH2_BASEURL_4,
-      userProfileURL: process.env.HD_AUTH_OAUTH2_USER_PROFILE_URL_4,
-      userProfileIdAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_ID_ATTR_4,
-      userProfileUsernameAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_USERNAME_ATTR_4,
-      userProfileDisplayNameAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_DISPLAY_NAME_ATTR_4,
-      userProfileEmailAttr:
-      process.env.HD_AUTH_OAUTH2_USER_PROFILE_EMAIL_ATTR_4,
-      tokenURL: process.env.HD_AUTH_OAUTH2_TOKEN_URL_4,
-      authorizationURL: process.env.HD_AUTH_OAUTH2_AUTHORIZATION_URL_4,
-      clientID: process.env.HD_AUTH_OAUTH2_CLIENT_ID_4,
-      clientSecret: process.env.HD_AUTH_OAUTH2_CLIENT_SECRET_4,
-      scope: process.env.HD_AUTH_OAUTH2_SCOPE_4,
-      rolesClaim: process.env.HD_AUTH_OAUTH2_ROLES_CLAIM_4,
-      accessRole: process.env.HD_AUTH_OAUTH2_ACCESS_ROLE_4,
-    },
-  ],
+  gitlab: gitlabs,
+  ldap: ldaps,
+  saml: samls,
+  oauth2: oauth2s,
 };
