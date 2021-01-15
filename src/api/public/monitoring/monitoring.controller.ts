8d89614a4d1cf0406246ed9174d0a0a975189531
Philip Molares <philip.molares@udo.edu>
2021-01-15 18:53:09 +0100

    auth: adds token-auth to public api
    adds auth service
    adds auth module
    adds token-auth strategy
    adds token-auth to all public api calls
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/api/public/monitoring/monitoring.controller.ts
+++ src/api/public/monitoring/monitoring.controller.ts
@@ -4,18 +4,21 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { MonitoringService } from '../../../monitoring/monitoring.service';
+import { TokenAuthGuard } from '../../../auth/token-auth.guard';
 
 @Controller('monitoring')
 export class MonitoringController {
   constructor(private monitoringService: MonitoringService) {}
 
+  @UseGuards(TokenAuthGuard)
   @Get()
   getStatus() {
     return this.monitoringService.getServerStatus();
   }
 
+  @UseGuards(TokenAuthGuard)
   @Get('prometheus')
   getPrometheusStatus() {
     return '';
