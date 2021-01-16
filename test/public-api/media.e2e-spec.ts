cce1626c48d14c3a2c3c2f584f6b814ae17eb9ce
Philip Molares <philip.molares@udo.edu>
2021-01-16 19:33:09 +0100

    auth: fixes unit and e2e tests
    adds MockAuthGuard which always return user 'hardcoded'
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- test/public-api/media.e2e-spec.ts
+++ test/public-api/media.e2e-spec.ts
@@ -21,6 +21,9 @@ import { NotesModule } from '../../src/notes/notes.module';
 import { NotesService } from '../../src/notes/notes.service';
 import { PermissionsModule } from '../../src/permissions/permissions.module';
 import { UsersService } from '../../src/users/users.service';
+import { AuthModule } from '../../src/auth/auth.module';
+import { TokenAuthGuard } from '../../src/auth/token-auth.guard';
+import { MockAuthGuard } from '../../src/auth/mock-auth.guard';
 
 describe('Notes', () => {
   let app: NestExpressApplication;
@@ -46,8 +49,12 @@ describe('Notes', () => {
         PermissionsModule,
         GroupsModule,
         LoggerModule,
+        AuthModule,
       ],
-    }).compile();
+    })
+      .overrideGuard(TokenAuthGuard)
+      .useClass(MockAuthGuard)
+      .compile();
     app = moduleRef.createNestApplication<NestExpressApplication>();
     app.useStaticAssets('uploads', {
       prefix: '/uploads',
