c3129d40e0cf4947db790c7b5a16e3adb49ee55c
David Mehren <git@herrmehren.de>
2021-02-20 20:41:42 +0100

    ESLint: Remove @typescript-eslint/interface-name-prefix rule
    The check was removed from typescript-eslint and replaced by the
    naming-convention rule.
    
    Signed-off-by: David Mehren <git@herrmehren.de>

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
     jest: true,
   },
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': [
       'warn',
