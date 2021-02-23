0e6cc0b971345799b5cbe5db9fbd65af19b63b08
Philip Molares <philip.molares@udo.edu>
2021-02-23 19:13:41 +0100

    PermissionsModule: Remove unused imports
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -5,10 +5,6 @@
  */
 
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { LoggerModule } from '../logger/logger.module';
-import { NoteGroupPermission } from './note-group-permission.entity';
-import { NoteUserPermission } from './note-user-permission.entity';
 import { PermissionsService } from './permissions.service';
 
 @Module({
