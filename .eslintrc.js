ba771fb7b6af28176bc46cbe6b4f15f0630e14d3
David Mehren <git@herrmehren.de>
2021-02-23 21:15:10 +0100

    Enable ESLint rules with type-checks
    Signed-off-by: David Mehren <git@herrmehren.de>

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
   ],
   root: true,
