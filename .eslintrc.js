2ba824d9e2ed418cb84e811e9d1be15b71f82d17
David Mehren <git@herrmehren.de>
2021-02-24 21:59:28 +0100

    ESLint: Re-enable @typescript-eslint/no-explicit-any rule
    Signed-off-by: David Mehren <git@herrmehren.de>

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': [
       'warn',
       { argsIgnorePattern: '^_+$' },
