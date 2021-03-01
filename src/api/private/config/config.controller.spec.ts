dd7ca87337def0994622fb85b212e354e6ce043a
Philip Molares <philip.molares@udo.edu>
2021-03-01 21:16:34 +0100

    PrivateApi: Add config controller
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

---
+++ src/api/private/config/config.controller.spec.ts
@@ -0,0 +1,44 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigController } from './config.controller';
+import { LoggerModule } from '../../../logger/logger.module';
+import { FrontendConfigModule } from '../../../frontend-config/frontend-config.module';
+import { ConfigModule } from '@nestjs/config';
+import appConfigMock from '../../../config/mock/app.config.mock';
+import authConfigMock from '../../../config/mock/auth.config.mock';
+import customizationConfigMock from '../../../config/mock/customization.config.mock';
+import externalConfigMock from '../../../config/mock/external-services.config.mock';
+
+describe('ConfigController', () => {
+  let controller: ConfigController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [
+            appConfigMock,
+            authConfigMock,
+            customizationConfigMock,
+            externalConfigMock,
+          ],
+        }),
+        LoggerModule,
+        FrontendConfigModule,
+      ],
+      controllers: [ConfigController],
+    }).compile();
+
+    controller = module.get<ConfigController>(ConfigController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
--- src/api/private/private-api.module.ts
+++ src/api/private/private-api.module.ts
@@ -9,12 +9,21 @@ import { TokensController } from './tokens/tokens.controller';
 import { LoggerModule } from '../../logger/logger.module';
 import { UsersModule } from '../../users/users.module';
 import { AuthModule } from '../../auth/auth.module';
+import { ConfigController } from './config/config.controller';
+import { FrontendConfigModule } from '../../frontend-config/frontend-config.module';
 import { HistoryController } from './me/history/history.controller';
 import { HistoryModule } from '../../history/history.module';
 import { NotesModule } from '../../notes/notes.module';
 
 @Module({
-  imports: [LoggerModule, UsersModule, AuthModule, HistoryModule, NotesModule],
-  controllers: [TokensController, HistoryController],
+  imports: [
+    LoggerModule,
+    UsersModule,
+    AuthModule,
+    FrontendConfigModule,
+    HistoryModule,
+    NotesModule,
+  ],
+  controllers: [TokensController, ConfigController, HistoryController],
 })
 export class PrivateApiModule {}
