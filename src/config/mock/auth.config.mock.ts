e3f1d1b0f46cb35fb2e81245a73847dca0e11bf4
Philip Molares <philip.molares@udo.edu>
2021-03-01 21:12:48 +0100

    Config: Create new config mocks for tests
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

---
+++ src/config/mock/auth.config.mock.ts
@@ -0,0 +1,40 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('authConfig', () => ({
+  email: {
+    enableLogin: true,
+    enableRegister: true,
+  },
+  facebook: {
+    clientID: undefined,
+    clientSecret: undefined,
+  },
+  twitter: {
+    consumerKey: undefined,
+    consumerSecret: undefined,
+  },
+  github: {
+    clientID: undefined,
+    clientSecret: undefined,
+  },
+  dropbox: {
+    clientID: undefined,
+    clientSecret: undefined,
+    appKey: undefined,
+  },
+  google: {
+    clientID: undefined,
+    clientSecret: undefined,
+    apiKey: undefined,
+  },
+  gitlab: [],
+  ldap: [],
+  saml: [],
+  oauth2: [],
+}));
