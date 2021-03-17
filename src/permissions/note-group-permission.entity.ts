029dc0d7d62eaeb8a87192e643ab555a2b875bc8
Philip Molares <philip.molares@udo.edu>
2021-03-17 10:26:42 +0100

    Permissions: Add cascade
    This makes it possible to create permissions by setting them in the note entity and delete them when either the user or note is deleted.
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/permissions/note-group-permission.entity.ts
+++ src/permissions/note-group-permission.entity.ts
@@ -10,10 +10,16 @@ import { Note } from '../notes/note.entity';
 
 @Entity()
 export class NoteGroupPermission {
-  @ManyToOne((_) => Group, { primary: true })
+  @ManyToOne((_) => Group, {
+    primary: true,
+    onDelete: 'CASCADE', // This deletes the NoteGroupPermission, when the associated Group is deleted
+  })
   group: Group;
 
-  @ManyToOne((_) => Note, (note) => note.groupPermissions, { primary: true })
+  @ManyToOne((_) => Note, (note) => note.groupPermissions, {
+    primary: true,
+    onDelete: 'CASCADE', // This deletes the NoteGroupPermission, when the associated Note is deleted
+  })
   note: Note;
 
   @Column()
