0e44f545a1de44fd3f7018d3ff975d2d5b7cce0d
Philip Molares <philip.molares@udo.edu>
2021-03-19 12:09:50 +0100

    Docs: Add description for common http codes
    These are the descriptions for all 401, 403, 404 and 204 HTTP responses in HedgeDoc.
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

---
+++ src/api/utils/descriptions.ts
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export const unauthorizedDescription =
+  'Authorization information is missing or invalid';
+export const forbiddenDescription =
+  'Access to the requested resource is not permitted';
+export const notFoundDescription = 'The requested resource was not found';
+export const successfullyDeletedDescription =
+  'The requested resource was sucessfully deleted';
