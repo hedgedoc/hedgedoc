0c25cebadff8557089e3f4413f501b856cb43edb
Yannick Bungers <git@innay.de>
2021-06-04 23:23:44 +0200

    Add basic cli functionality for managing users
    Signed-off-by: Yannick Bungers <git@innay.de>

--- src/cli.ts
+++ src/cli.ts
@@ -7,11 +7,15 @@
 import { NestFactory } from '@nestjs/core';
 import { CommandModule, CommandService } from 'nestjs-command';
 import { AppModule } from './app.module';
+import { ConsoleLoggerService } from './logger/console-logger.service';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.createApplicationContext(AppModule, {
     logger: ['error'], // only errors
   });
+  const logger = await app.resolve(ConsoleLoggerService);
+  logger.log('Switching logger', 'AppBootstrap');
+  app.useLogger(logger);
   app.select(CommandModule).get(CommandService).exec();
 }
 
--- src/cli/cli.module.ts
+++ src/cli/cli.module.ts
@@ -8,10 +8,13 @@ import { Module, OnModuleInit } from '@nestjs/common';
 import { CommandModule, CommandService } from 'nestjs-command';
 import { ConfigModule } from '@nestjs/config';
 import { ConfigCommand } from './config.command';
+import { UserCommand } from './user.command';
+import { UsersModule } from '../users/users.module';
+import { LoggerModule } from '../logger/logger.module';
 
 @Module({
-  imports: [CommandModule, ConfigModule],
-  providers: [ConfigCommand],
+  imports: [CommandModule, ConfigModule, UsersModule, LoggerModule],
+  providers: [ConfigCommand, UserCommand],
 })
 export class CliModule implements OnModuleInit {
   constructor(private readonly command: CommandService) {}
