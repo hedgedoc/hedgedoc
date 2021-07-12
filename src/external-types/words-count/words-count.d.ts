888f2bd95db7d961d80f0111c0cf41200c99b3ec
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-07-12 17:22:27 +0200

    Remove custom types for words-count
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/external-types/words-count/words-count.d.ts
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
- *
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-
-declare module 'words-count' {
-  export default function wordsCount(text: string): number
-}
