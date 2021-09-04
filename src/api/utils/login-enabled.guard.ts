cd4ee84ec31e52639f34d2121d757b3dc8618ff8
Philip Molares <philip.molares@udo.edu>
2021-09-04 18:31:01 +0200

    feat: add LoginEnabledGuard and RegistrationEnabledGuard
    These guards check if the login or registration are enabled in the config. If so the guarded method is executed, if not the client will get the HTTP Error 400 Forbidden as an answer
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

---
+++ src/api/utils/login-enabled.guard.ts
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import {
+  BadRequestException,
+  CanActivate,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+
+import authConfiguration, { AuthConfig } from '../../config/auth.config';
+import { ConsoleLoggerService } from '../../logger/console-logger.service';
+
+@Injectable()
+export class LoginEnabledGuard implements CanActivate {
+  constructor(
+    private readonly logger: ConsoleLoggerService,
+    @Inject(authConfiguration.KEY)
+    private authConfig: AuthConfig,
+  ) {
+    this.logger.setContext(LoginEnabledGuard.name);
+  }
+
+  canActivate(): boolean {
+    if (!this.authConfig.local.enableLogin) {
+      this.logger.debug('Local auth is disabled.', 'canActivate');
+      throw new BadRequestException('Local auth is disabled.');
+    }
+    return true;
+  }
+}
