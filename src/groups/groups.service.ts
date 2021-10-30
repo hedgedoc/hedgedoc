371c7dfe111bbc921ce021c535bfd573fc3178ba
Philip Molares <philip.molares@udo.edu>
2021-10-31 00:04:12 +0200

    feat: add createGroup function
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { NotInDBError } from '../errors/errors';
+import { AlreadyInDBError, NotInDBError } from '../errors/errors';
 import { ConsoleLoggerService } from '../logger/console-logger.service';
 import { GroupInfoDto } from './group-info.dto';
 import { Group } from './group.entity';
@@ -19,6 +19,35 @@ export class GroupsService {
     @InjectRepository(Group) private groupRepository: Repository<Group>,
   ) {
     this.logger.setContext(GroupsService.name);
+  }
+
+  /**
+   * @async
+   * Create a new group with a given name and displayName
+   * @param name - the group name the new group shall have
+   * @param displayName - the display name the new group shall have
+   * @param special - if the group is special or not
+   * @return {Group} the group
+   * @throws {AlreadyInDBError} the group name is already taken.
+   */
+  async createGroup(
+    name: string,
+    displayName: string,
+    special = false,
+  ): Promise<Group> {
+    const group = Group.create(name, displayName);
+    group.special = special;
+    try {
+      return await this.groupRepository.save(group);
+    } catch {
+      this.logger.debug(
+        `A group with the name '${name}' already exists.`,
+        'createGroup',
+      );
+      throw new AlreadyInDBError(
+        `A group with the name '${name}' already exists.`,
+      );
+    }
   }
 
   /**
