abceb356db6683be94edb37230d9a753359a8e17
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-11-20 19:10:36 +0100

    format code
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/components/application-loader/initializers/i18n/i18n.ts
+++ src/components/application-loader/initializers/i18n/i18n.ts
@@ -5,7 +5,7 @@
  */
 
 import type { ResourceKey } from 'i18next'
-import i18n, { use as i18nUse }  from 'i18next'
+import i18n, { use as i18nUse } from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import resourcesToBackend from 'i18next-resources-to-backend'
 import { Settings } from 'luxon'
@@ -13,16 +13,16 @@ import { initReactI18next } from 'react-i18next'
 
 export const setUpI18n = async (): Promise<void> => {
   await i18nUse(
-      resourcesToBackend((language, namespace, callback) => {
-        import(`../../../../../locales/${language}.json`)
-          .then((resources: ResourceKey) => {
-            callback(null, resources)
-          })
-          .catch((error: Error) => {
-            callback(error, null)
-          })
-      })
-    )
+    resourcesToBackend((language, namespace, callback) => {
+      import(`../../../../../locales/${language}.json`)
+        .then((resources: ResourceKey) => {
+          callback(null, resources)
+        })
+        .catch((error: Error) => {
+          callback(error, null)
+        })
+    })
+  )
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
