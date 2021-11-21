6f7cfced3917b283367b7b5abf62109595a5d7ab
Philip Molares <philip.molares@udo.edu>
2021-11-21 17:17:33 +0100

    feat: create permission decorator
    This gathers the permission a user needs to hold to access a resource for the PermissionsGuard.
    
    See https://docs.nestjs.com/guards#setting-roles-per-handler
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

---
+++ src/permissions/permissions.decorator.ts
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { CustomDecorator, SetMetadata } from '@nestjs/common';
+
+import { Permission } from './permissions.enum';
+
+/**
+ * This decorator gathers the {@link Permission Permission} a user must hold for the {@link PermissionsGuard}
+ * @param permissions - an array of permissions. In practice this should always contain exactly one {@link Permission}
+ * @constructor
+ */
+// eslint-disable-next-line func-style,@typescript-eslint/naming-convention
+export const Permissions = (...permissions: Permission[]): CustomDecorator =>
+  SetMetadata('permissions', permissions);
