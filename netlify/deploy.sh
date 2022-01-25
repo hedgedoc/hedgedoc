523701b3d2c694a095a5384cf15f0b30c7a974ba
Erik Michelson <github@erik.michelson.eu>
2022-01-25 16:10:34 +0100

    Add netlify deployments for PRs
    Co-authored-by: Tilman Vatteroth <git@tilmanvatteroth.de>
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

---
+++ netlify/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+deployid="$1-$2"
+
+json=$(yarn netlify deploy --build --context deploy-preview --alias "${deployid}" --json)
+
+url=$(echo "${json}" | jq -r .deploy_url)
+logs=$(echo "${json}" | jq -r .logs)
+
+echo "::set-output name=url::${url}"
+echo "::set-output name=logs::${logs}"
