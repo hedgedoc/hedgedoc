2d8d29cf203bb5626e3ba69bf462360c8539d063
Philip Molares <philip.molares@udo.edu>
2022-01-30 15:51:18 +0100

    refactor(config): type config mocks
    To minimize type errors, when the config objects are changed, this commit introduces types to the mock config object accordingly.
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/config/mock/auth.config.mock.ts
+++ src/config/mock/auth.config.mock.ts
@@ -1,43 +1,48 @@
 /*
- * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('authConfig', () => ({
-  session: {
-    secret: 'my_secret',
-    lifetime: 1209600000,
-  },
-  local: {
-    enableLogin: true,
-    enableRegister: true,
-  },
-  facebook: {
-    clientID: undefined,
-    clientSecret: undefined,
-  },
-  twitter: {
-    consumerKey: undefined,
-    consumerSecret: undefined,
-  },
-  github: {
-    clientID: undefined,
-    clientSecret: undefined,
-  },
-  dropbox: {
-    clientID: undefined,
-    clientSecret: undefined,
-    appKey: undefined,
-  },
-  google: {
-    clientID: undefined,
-    clientSecret: undefined,
-    apiKey: undefined,
-  },
-  gitlab: [],
-  ldap: [],
-  saml: [],
-  oauth2: [],
-}));
+import { AuthConfig } from '../auth.config';
+
+export default registerAs(
+  'authConfig',
+  (): AuthConfig => ({
+    session: {
+      secret: 'my_secret',
+      lifetime: 1209600000,
+    },
+    local: {
+      enableLogin: true,
+      enableRegister: true,
+    },
+    facebook: {
+      clientID: '',
+      clientSecret: '',
+    },
+    twitter: {
+      consumerKey: '',
+      consumerSecret: '',
+    },
+    github: {
+      clientID: '',
+      clientSecret: '',
+    },
+    dropbox: {
+      clientID: '',
+      clientSecret: '',
+      appKey: '',
+    },
+    google: {
+      clientID: '',
+      clientSecret: '',
+      apiKey: '',
+    },
+    gitlab: [],
+    ldap: [],
+    saml: [],
+    oauth2: [],
+  }),
+);
