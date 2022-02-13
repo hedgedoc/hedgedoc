a9f27731bd005fa261d0c89e672b7c9eec438c76
David Mehren <git@herrmehren.de>
2022-02-13 19:39:05 +0100

    refactor(revision): make RevisionDto extend RevisionMetadataDto
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/revisions/revision.dto.ts
+++ src/revisions/revision.dto.ts
@@ -4,22 +4,12 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { ApiProperty } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
-import { IsDate, IsNumber, IsString, ValidateNested } from 'class-validator';
+import { IsString, ValidateNested } from 'class-validator';
 
-import { BaseDto } from '../utils/base.dto.';
 import { EditDto } from './edit.dto';
-import { Revision } from './revision.entity';
+import { RevisionMetadataDto } from './revision-metadata.dto';
 
-export class RevisionDto extends BaseDto {
-  /**
-   * ID of this revision
-   * @example 13
-   */
-  @IsNumber()
-  @ApiProperty()
-  id: Revision['id'];
-
+export class RevisionDto extends RevisionMetadataDto {
   /**
    * Markdown content of the revision
    * @example "# I am a heading"
@@ -29,28 +19,11 @@ export class RevisionDto extends BaseDto {
   content: string;
 
   /**
-   * Number of characters in this revision
-   * @example 142
-   */
-  @IsNumber()
-  @ApiProperty()
-  length: number;
-
-  /**
    * Patch from the preceding revision to this one
    */
   @IsString()
   @ApiProperty()
   patch: string;
-
-  /**
-   * Datestring of the time this revision was created
-   * @example "2020-12-01 12:23:34"
-   */
-  @IsDate()
-  @Type(() => Date)
-  @ApiProperty()
-  createdAt: Date;
 
   /**
    * All edit objects which are used in the revision.
