f2958a77feb3dd7bec878f6ad648d92fb793cb75
Tilman Vatteroth <git@tilmanvatteroth.de>
2022-03-28 22:01:54 +0200

    Add markdown-extension-collection.ts
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/components/markdown-renderer/hooks/use-convert-markdown-to-react-dom.ts
+++ src/components/markdown-renderer/hooks/use-convert-markdown-to-react-dom.ts
@@ -6,17 +6,15 @@
 
 import MarkdownIt from 'markdown-it/lib'
 import { useMemo } from 'react'
-import type { ComponentReplacer, ValidReactDomElement } from '../replace-components/component-replacer'
+import type { ValidReactDomElement } from '../replace-components/component-replacer'
 import convertHtmlToReact from '@hedgedoc/html-to-react'
 import { NodeToReactTransformer } from '../utils/node-to-react-transformer'
 import { LineIdMapper } from '../utils/line-id-mapper'
 import type { MarkdownExtension } from '../markdown-extension/markdown-extension'
-import type { NodeProcessor } from '../node-preprocessors/node-processor'
-import type { Document } from 'domhandler'
-import { SanitizerMarkdownExtension } from '../markdown-extension/sanitizer/sanitizer-markdown-extension'
+import { MarkdownExtensionCollection } from '../markdown-extension/markdown-extension-collection'
 
 /**
- * Renders markdown code into react elements
+ * Renders Markdown-Code into react elements
  *
  * @param markdownContentLines The markdown code lines that should be rendered
  * @param additionalMarkdownExtensions A list of {@link MarkdownExtension markdown extensions} that should be used
@@ -32,13 +30,10 @@ export const useConvertMarkdownToReactDom = (
 ): ValidReactDomElement[] => {
   const lineNumberMapper = useMemo(() => new LineIdMapper(), [])
   const htmlToReactTransformer = useMemo(() => new NodeToReactTransformer(), [])
-  const markdownExtensions = useMemo(() => {
-    const tagNameWhiteList = additionalMarkdownExtensions.reduce(
-      (state, extension) => [...state, ...extension.buildTagNameWhitelist()],
-      [] as string[]
-    )
-    return [...additionalMarkdownExtensions, new SanitizerMarkdownExtension(tagNameWhiteList)]
-  }, [additionalMarkdownExtensions])
+  const markdownExtensions = useMemo(
+    () => new MarkdownExtensionCollection(additionalMarkdownExtensions),
+    [additionalMarkdownExtensions]
+  )
 
   const markdownIt = useMemo(() => {
     const newMarkdownIt = new MarkdownIt('default', {
@@ -47,35 +42,19 @@ export const useConvertMarkdownToReactDom = (
       langPrefix: '',
       typographer: true
     })
-    markdownExtensions.forEach((extension) =>
-      newMarkdownIt.use((markdownIt) => extension.configureMarkdownIt(markdownIt))
-    )
-    markdownExtensions.forEach((extension) =>
-      newMarkdownIt.use((markdownIt) => extension.configureMarkdownItPost(markdownIt))
-    )
+    markdownExtensions.configureMarkdownIt(newMarkdownIt)
     return newMarkdownIt
   }, [allowHtml, markdownExtensions, newlinesAreBreaks])
 
   useMemo(() => {
-    const replacers = markdownExtensions.reduce(
-      (state, extension) => [...state, ...extension.buildReplacers()],
-      [] as ComponentReplacer[]
-    )
-    htmlToReactTransformer.setReplacers(replacers)
+    htmlToReactTransformer.setReplacers(markdownExtensions.buildReplacers())
   }, [htmlToReactTransformer, markdownExtensions])
 
   useMemo(() => {
     htmlToReactTransformer.setLineIds(lineNumberMapper.updateLineMapping(markdownContentLines))
   }, [htmlToReactTransformer, lineNumberMapper, markdownContentLines])
 
-  const nodePreProcessor = useMemo(() => {
-    return markdownExtensions
-      .reduce((state, extension) => [...state, ...extension.buildNodeProcessors()], [] as NodeProcessor[])
-      .reduce(
-        (state, processor) => (document: Document) => state(processor.process(document)),
-        (document: Document) => document
-      )
-  }, [markdownExtensions])
+  const nodePreProcessor = useMemo(() => markdownExtensions.buildFlatNodeProcessor(), [markdownExtensions])
 
   return useMemo(() => {
     const html = markdownIt.render(markdownContentLines.join('\n'))
