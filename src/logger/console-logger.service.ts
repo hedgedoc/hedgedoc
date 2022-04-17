hedgedoc/hedgedoc

commit 2467b1250fff619059299a69cec0d8e4326c984e

feat: add sanitization to logger service
Signed-off-by: Philip Molares <philip.molares@udo.edu>

src/logger/console-logger.service.ts
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
@@ -101,15 +101,30 @@ export class ConsoleLoggerService implements LoggerService {
     return context;
   }
 
+  static sanitize(input: string): string {
+    return (
+      input
+        // remove ASCII control characters
+        .replace(/\p{C}/gu, '')
+        // replace all non-zeros width spaces with one space
+        .replace(/\p{Zs}/gu, ' ')
+    );
+  }
+
   private printMessage(
     message: unknown,
     color: (message: string) => string,
     context = '',
     isTimeDiffEnabled?: boolean,
   ): void {
-    const output = isObject(message)
-      ? `${color('Object:')}\n${JSON.stringify(message, null, 2)}\n`
-      : color(message as string);
+    let output;
+    if (isObject(message)) {
+      output = ConsoleLoggerService.sanitize(
+        `${color('Object:')}\n${JSON.stringify(message, null, 2)}\n`,
+      );
+    } else {
+      output = ConsoleLoggerService.sanitize(color(message as string));
+    }
 
     const localeStringOptions: DateTimeFormatOptions = {
       year: 'numeric',

commit da408d59dc28f2fa963d7ac676543e36c6624dcc

fix(console-logger): don't sanitize colors away
2467b125 mistakenly applied the sanitize function
to the log messages *after* the color was applied.

This commit reverses the order to un-break colored logs.

Signed-off-by: David Mehren <git@herrmehren.de>

src/logger/console-logger.service.ts
@@ -119,11 +119,11 @@ export class ConsoleLoggerService implements LoggerService {
   ): void {
     let output;
     if (isObject(message)) {
-      output = ConsoleLoggerService.sanitize(
-        `${color('Object:')}\n${JSON.stringify(message, null, 2)}\n`,
-      );
+      output = `${color('Object:')}\n${ConsoleLoggerService.sanitize(
+        JSON.stringify(message, null, 2),
+      )}\n`;
     } else {
-      output = ConsoleLoggerService.sanitize(color(message as string));
+      output = color(ConsoleLoggerService.sanitize(message as string));
     }
 
     const localeStringOptions: DateTimeFormatOptions = {