f557901944308bb3fa9b108b6a66f1dc9dfeeed7
Philip Molares <philip.molares@udo.edu>
2022-05-21 16:26:47 +0200

    test: add tests for show-if
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

---
+++ src/components/common/show-if/show-if.test.tsx
@@ -0,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { render } from '@testing-library/react'
+import { ShowIf } from './show-if'
+
+describe('ShowIf', () => {
+  it('renders child if condition is true', () => {
+    const view = render(<ShowIf condition={true}>test</ShowIf>)
+    expect(view.container).toMatchSnapshot()
+  })
+
+  it('does not render child if condition is false', () => {
+    const view = render(<ShowIf condition={false}>test</ShowIf>)
+    expect(view.container).toMatchSnapshot()
+  })
+})
