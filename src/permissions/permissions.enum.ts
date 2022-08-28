bdedf783ec17c75915c9f01e03bf9e1124ccb72e
Philip Molares <philip.molares@udo.edu>
2022-08-28 21:35:22 +0200

    refactor: remove additional copyright text
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/permissions/permissions.enum.ts
+++ src/permissions/permissions.enum.ts
@@ -1,11 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
- *
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-
-/*
- * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
