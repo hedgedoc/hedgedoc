a9435e365215b2fabcb7a8a7b0c1339cb721e340
Tilman Vatteroth <git@tilmanvatteroth.de>
2022-09-25 15:17:08 +0200

    Replace @matejmazur/react-katex with own katex component (#2381)
    * Replace @matejmazur/react-katex with self-made katex component
    
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

---
+++ src/components/markdown-renderer/markdown-extension/katex/katex-frame.test.tsx
@@ -0,0 +1,65 @@
+/*
+ * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { render } from '@testing-library/react'
+import KatexFrame from './katex-frame'
+import type { KatexOptions } from 'katex'
+import { default as KatexDefault } from 'katex'
+
+jest.mock('katex')
+
+describe('katex frame', () => {
+  afterAll(() => {
+    jest.resetAllMocks()
+    jest.resetModules()
+  })
+
+  beforeEach(() => {
+    jest.spyOn(KatexDefault, 'renderToString').mockImplementation(
+      (tex: string, options?: KatexOptions) => `<span>This is a mock for lib katex with this parameters:</span>
+<ul>
+  <li>tex: ${tex}</li>
+  <li>block: ${String(options?.displayMode)}</li>
+</ul>`
+    )
+  })
+
+  describe('renders a valid latex expression', () => {
+    it('as implicit inline', () => {
+      const view = render(<KatexFrame expression={'\\int_0^\\infty x^2 dx'}></KatexFrame>)
+      expect(view.container).toMatchSnapshot()
+    })
+    it('as explicit inline', () => {
+      const view = render(<KatexFrame expression={'\\int_0^\\infty x^2 dx'} block={false}></KatexFrame>)
+      expect(view.container).toMatchSnapshot()
+    })
+    it('as explicit block', () => {
+      const view = render(<KatexFrame expression={'\\int_0^\\infty x^2 dx'} block={true}></KatexFrame>)
+      expect(view.container).toMatchSnapshot()
+    })
+  })
+
+  describe('renders an error for an invalid latex expression', () => {
+    beforeEach(() => {
+      jest.spyOn(KatexDefault, 'renderToString').mockImplementation(() => {
+        throw new Error('mocked parseerror')
+      })
+    })
+
+    it('as implicit inline', () => {
+      const view = render(<KatexFrame expression={'\\alf'}></KatexFrame>)
+      expect(view.container).toMatchSnapshot()
+    })
+    it('as explicit inline', () => {
+      const view = render(<KatexFrame expression={'\\alf'} block={false}></KatexFrame>)
+      expect(view.container).toMatchSnapshot()
+    })
+    it('as explicit block', () => {
+      const view = render(<KatexFrame expression={'\\alf'} block={true}></KatexFrame>)
+      expect(view.container).toMatchSnapshot()
+    })
+  })
+})
---
+++ src/components/markdown-renderer/markdown-extension/katex/katex-frame.tsx
@@ -0,0 +1,46 @@
+/*
+ * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import React, { useMemo } from 'react'
+import KaTeX from 'katex'
+import convertHtmlToReact from '@hedgedoc/html-to-react'
+import 'katex/dist/katex.min.css'
+import { Alert } from 'react-bootstrap'
+import { testId } from '../../../../utils/test-id'
+import { sanitize } from 'dompurify'
+
+interface KatexFrameProps {
+  expression: string
+  block?: boolean
+}
+
+/**
+ * Renders a LaTeX expression.
+ *
+ * @param expression The mathematical expression to render
+ * @param block Defines if the output should be a block or inline.
+ */
+export const KatexFrame: React.FC<KatexFrameProps> = ({ expression, block = false }) => {
+  const dom = useMemo(() => {
+    try {
+      const katexHtml = KaTeX.renderToString(expression, {
+        displayMode: block === true,
+        throwOnError: true
+      })
+      return convertHtmlToReact(sanitize(katexHtml, { ADD_TAGS: ['semantics', 'annotation'] }))
+    } catch (error) {
+      return (
+        <Alert className={block ? '' : 'd-inline-block'} variant={'danger'}>
+          {(error as Error).message}
+        </Alert>
+      )
+    }
+  }, [block, expression])
+
+  return block ? <div {...testId('katex-block')}>{dom}</div> : <span {...testId('katex-inline')}>{dom}</span>
+}
+
+export default KatexFrame
