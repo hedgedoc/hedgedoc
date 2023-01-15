5f238e96518495cd8174c336d08c9055ea572203
Philip Molares <philip.molares@udo.edu>
2023-01-15 22:14:03 +0100

    docs(frontend): expand CreateNonExistingNoteHint docs
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- frontend/src/components/common/note-loading-boundary/create-non-existing-note-hint.tsx
+++ frontend/src/components/common/note-loading-boundary/create-non-existing-note-hint.tsx
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
+ * SPDX-FileCopyrightText: 2023 The HedgeDoc developers (see AUTHORS file)
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
@@ -20,6 +20,8 @@ export interface CreateNonExistingNoteHintProps {
 /**
  * Shows a button that creates an empty note with the alias from the current window URL.
  * When the button was clicked it also shows the progress.
+ *
+ * @param onNoteCreated A function that will be called after the note was created.
  */
 export const CreateNonExistingNoteHint: React.FC<CreateNonExistingNoteHintProps> = ({ onNoteCreated }) => {
   useTranslation()
