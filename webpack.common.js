a44ae8b27b880bbdc5f41ab946c46c7f0dcdc722
Tilman Vatteroth <git@tilmanvatteroth.de>
2023-02-17 18:19:37 +0100

    feat(bundle): add webpack clean plugin
    To avoid unnecessary and broken files in the build directory.
    
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 // Fix possible nofile-issues
 const fs = require('fs')
@@ -12,6 +13,7 @@ gracefulFs.gracefulify(fs)
 module.exports = {
   name: 'app',
   plugins: [
+    new CleanWebpackPlugin(),
     new webpack.ProvidePlugin({
       Visibility: 'visibilityjs',
       Cookies: 'js-cookie',
