efc1ff6d9347db1cf25e148a2f117cb08b3e5a8d
Philip Molares <philip.molares@udo.edu>
2023-03-24 18:17:49 +0100

    feat(frontend): deactivate delete note button if user is not owner
    This button and its functionality only works if the user is the owner, so it doesn't make sense to make it possible to press it otherwise…
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- frontend/src/components/common/modals/deletion-moadal.test.tsx
+++ frontend/src/components/common/modals/deletion-moadal.test.tsx
@@ -1,15 +1,37 @@
 /*
- * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
+ * SPDX-FileCopyrightText: 2023 The HedgeDoc developers (see AUTHORS file)
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import { mockNoteOwnership } from '../../../test-utils/note-ownership'
 import { mockI18n } from '../../markdown-renderer/test-utils/mock-i18n'
 import { DeletionModal } from './deletion-modal'
 import { render, screen } from '@testing-library/react'
 
 describe('DeletionModal', () => {
-  it('renders correctly with deletionButtonI18nKey', async () => {
+  beforeEach(async () => {
     await mockI18n()
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+    jest.resetModules()
+  })
+
+  it('renders correctly with deletionButtonI18nKey', async () => {
+    mockNoteOwnership('test', 'test')
+    const onConfirm = jest.fn()
+    render(
+      <DeletionModal onConfirm={onConfirm} deletionButtonI18nKey={'testDeletionButton'} show={true}>
+        testText
+      </DeletionModal>
+    )
+    const modal = await screen.findByTestId('commonModal')
+    expect(modal).toMatchSnapshot()
+  })
+
+  it('disables deletion when user is not owner', async () => {
+    mockNoteOwnership('test2', 'test')
     const onConfirm = jest.fn()
     render(
       <DeletionModal onConfirm={onConfirm} deletionButtonI18nKey={'testDeletionButton'} show={true}>
