e97a426680f096c3fa129ef13e3f69fc1a4999a4
Tilman Vatteroth <git@tilmanvatteroth.de>
2023-03-24 13:16:35 +0100

    refactor: split avatar component to handle displaynames
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- frontend/src/components/common/user-avatar/user-avatar-for-username.tsx
+++ frontend/src/components/common/user-avatar/user-avatar-for-username.tsx
@@ -4,15 +4,14 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { getUser } from '../../../api/users'
-import type { UserInfo } from '../../../api/users/types'
 import { AsyncLoadingBoundary } from '../async-loading-boundary/async-loading-boundary'
 import type { UserAvatarProps } from './user-avatar'
 import { UserAvatar } from './user-avatar'
-import React from 'react'
+import React, { Fragment, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useAsync } from 'react-use'
 
-export interface UserAvatarForUsernameProps extends Omit<UserAvatarProps, 'user'> {
+export interface UserAvatarForUsernameProps extends Omit<UserAvatarProps, 'photoUrl' | 'displayName'> {
   username: string | null
 }
 
@@ -27,20 +26,21 @@ export interface UserAvatarForUsernameProps extends Omit<UserAvatarProps, 'user'
  */
 export const UserAvatarForUsername: React.FC<UserAvatarForUsernameProps> = ({ username, ...props }) => {
   const { t } = useTranslation()
-  const { error, value, loading } = useAsync(async (): Promise<UserInfo> => {
-    if (username) {
-      return await getUser(username)
-    }
-    return {
-      displayName: t('common.guestUser'),
-      photo: `public/img/avatar.png`,
-      username: ''
-    }
+  const { error, value, loading } = useAsync(async (): Promise<{ displayName: string; photo?: string }> => {
+    return username
+      ? await getUser(username)
+      : {
+          displayName: t('common.guestUser')
+        }
   }, [username, t])
+
+  const avatar = useMemo(() => {
+    return !value ? <Fragment /> : <UserAvatar displayName={value.displayName} photoUrl={value.photo} {...props} />
+  }, [props, value])
 
   return (
     <AsyncLoadingBoundary loading={loading || !value} error={error} componentName={'UserAvatarForUsername'}>
-      <UserAvatar user={value as UserInfo} {...props} />
+      {avatar}
     </AsyncLoadingBoundary>
   )
 }
--- frontend/src/components/editor-page/sidebar/user-line/user-line.tsx
+++ frontend/src/components/editor-page/sidebar/user-line/user-line.tsx
@@ -3,14 +3,16 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import { UserAvatar } from '../../../common/user-avatar/user-avatar'
 import { UserAvatarForUsername } from '../../../common/user-avatar/user-avatar-for-username'
 import { createCursorCssClass } from '../../editor-pane/codemirror-extensions/remote-cursors/create-cursor-css-class'
 import { ActiveIndicator } from '../users-online-sidebar-menu/active-indicator'
 import styles from './user-line.module.scss'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 export interface UserLineProps {
   username: string | null
+  displayName: string
   active: boolean
   color: number
 }
@@ -22,7 +24,22 @@ export interface UserLineProps {
  * @param color The color of the user's edits.
  * @param status The user's current online status.
  */
-export const UserLine: React.FC<UserLineProps> = ({ username, active, color }) => {
+export const UserLine: React.FC<UserLineProps> = ({ username, displayName, active, color }) => {
+  const avatar = useMemo(() => {
+    if (username) {
+      return (
+        <UserAvatarForUsername
+          username={username}
+          additionalClasses={'flex-fill overflow-hidden px-2 text-nowrap w-100'}
+        />
+      )
+    } else {
+      return (
+        <UserAvatar displayName={displayName} additionalClasses={'flex-fill overflow-hidden px-2 text-nowrap w-100'} />
+      )
+    }
+  }, [displayName, username])
+
   return (
     <div className={'d-flex align-items-center h-100 w-100'}>
       <div
@@ -30,10 +47,7 @@ export const UserLine: React.FC<UserLineProps> = ({ username, active, color }) =
           color
         )}`}
       />
-      <UserAvatarForUsername
-        username={username}
-        additionalClasses={'flex-fill overflow-hidden px-2 text-nowrap w-100'}
-      />
+      {avatar}
       <div className={styles['active-indicator-container']}>
         <ActiveIndicator active={active} />
       </div>
