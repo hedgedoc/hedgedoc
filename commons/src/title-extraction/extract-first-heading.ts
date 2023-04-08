c6ce0bc7044bedf00ee3f34d6b16417dff3802a0
Tilman Vatteroth <git@tilmanvatteroth.de>
2023-04-08 21:31:27 +0200

    feat: move title extraction into commons package
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

---
+++ commons/src/title-extraction/extract-first-heading.ts
@@ -0,0 +1,57 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { Element, isTag, isText, Node, NodeWithChildren } from 'domhandler'
+
+const headlineTagRegex = /^h[1-6]$/gi
+
+/**
+ * Extracts the text content of the first top level headline tag.
+ *
+ * @param nodes The node whose children should be checked for the headline
+ * @return the plain text representation of the first headline. {@code undefined} if no headline has been found.
+ */
+export function extractFirstHeading(
+  nodes: NodeWithChildren
+): string | undefined {
+  const foundHeadlineNode = checkNodesForHeadline(nodes.children)
+  if (!foundHeadlineNode) {
+    return
+  }
+  return extractInnerTextFromNode(foundHeadlineNode).trim()
+}
+
+function checkNodesForHeadline(nodes: Node[]): Node | undefined {
+  return nodes.find((node) => isTag(node) && node.name.match(headlineTagRegex))
+}
+
+function extractInnerTextFromNode(node: Node): string {
+  if (isText(node)) {
+    return node.nodeValue
+  } else if (isTag(node)) {
+    return extractInnerTextFromTag(node)
+  } else {
+    return ''
+  }
+}
+
+function extractInnerTextFromTag(node: Element): string {
+  if (
+    node.name === 'a' &&
+    findAttribute(node, 'class')?.value.split(' ').includes('heading-anchor')
+  ) {
+    return ''
+  } else if (node.name === 'img') {
+    return findAttribute(node, 'alt')?.value ?? ''
+  } else {
+    return node.children.reduce((state, child) => {
+      return state + extractInnerTextFromNode(child)
+    }, '')
+  }
+}
+
+function findAttribute(node: Element, attributeName: string) {
+  return node.attributes.find((attribute) => attribute.name === attributeName)
+}
--- frontend/src/components/markdown-renderer/extensions/extract-first-headline/extract-first-headline-node-processor.ts
+++ frontend/src/components/markdown-renderer/extensions/extract-first-headline/extract-first-headline-node-processor.ts
@@ -4,12 +4,10 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { NodeProcessor } from '../../node-preprocessors/node-processor'
+import { extractFirstHeading } from '@hedgedoc/commons'
 import { Optional } from '@mrdrogdrog/optional'
-import type { Document, Node, Element } from 'domhandler'
-import { isTag, isText } from 'domhandler'
+import type { Document } from 'domhandler'
 import type { EventEmitter2 } from 'eventemitter2'
-
-const headlineTagRegex = /^h[1-6]$/gi
 
 /**
  * Searches for the first headline tag and extracts its plain text content.
@@ -22,40 +20,9 @@ export class ExtractFirstHeadlineNodeProcessor extends NodeProcessor {
   }
 
   process(nodes: Document): Document {
-    Optional.ofNullable(this.checkNodesForHeadline(nodes.children))
-      .map((foundHeadlineNode) => this.extractInnerTextFromNode(foundHeadlineNode).trim())
+    Optional.ofNullable(extractFirstHeading(nodes))
       .filter((text) => text !== '')
       .ifPresent((text) => this.eventEmitter.emit(ExtractFirstHeadlineNodeProcessor.EVENT_NAME, text))
     return nodes
-  }
-
-  private checkNodesForHeadline(nodes: Node[]): Node | undefined {
-    return nodes.find((node) => isTag(node) && node.name.match(headlineTagRegex))
-  }
-
-  private extractInnerTextFromNode(node: Node): string {
-    if (isText(node)) {
-      return node.nodeValue
-    } else if (isTag(node)) {
-      return this.extractInnerTextFromTag(node)
-    } else {
-      return ''
-    }
-  }
-
-  private extractInnerTextFromTag(node: Element): string {
-    if (node.name === 'a' && this.findAttribute(node, 'class')?.value.includes('heading-anchor')) {
-      return ''
-    } else if (node.name === 'img') {
-      return this.findAttribute(node, 'alt')?.value ?? ''
-    } else {
-      return node.children.reduce((state, child) => {
-        return state + this.extractInnerTextFromNode(child)
-      }, '')
-    }
-  }
-
-  private findAttribute(node: Element, attributeName: string) {
-    return node.attributes.find((attribute) => attribute.name === attributeName)
   }
 }
