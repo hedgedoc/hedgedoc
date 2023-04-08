7b28e244e93791a0978c575cdb360e4008b77666
Tilman Vatteroth <git@tilmanvatteroth.de>
2023-04-08 18:15:48 +0200

    feat(renderer): Add intermediate class for markdown renderer extensions with event emitter
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- frontend/src/components/markdown-renderer/extensions/base/markdown-renderer-extension.ts
+++ frontend/src/components/markdown-renderer/extensions/base/markdown-renderer-extension.ts
@@ -5,15 +5,12 @@
  */
 import type { NodeProcessor } from '../../node-preprocessors/node-processor'
 import type { ComponentReplacer } from '../../replace-components/component-replacer'
-import type { EventEmitter2 } from 'eventemitter2'
 import type MarkdownIt from 'markdown-it'
 
 /**
  * Base class for Markdown extensions.
  */
 export abstract class MarkdownRendererExtension {
-  constructor(protected readonly eventEmitter?: EventEmitter2) {}
-
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public configureMarkdownIt(markdownIt: MarkdownIt): void {
     return
