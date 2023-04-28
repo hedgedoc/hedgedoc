3f96616b5aed4744c1b9cbff1e4a51084e483131
Tilman Vatteroth <git@tilmanvatteroth.de>
2023-04-28 21:58:53 +0200

    fix: remove not migrated language files
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- frontend/src/components/layout/settings-dialog/global/available-languages.ts
+++ frontend/src/components/layout/settings-dialog/global/available-languages.ts
@@ -4,33 +4,4 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-export const availableLanguages = () => [
-  'en',
-  'zh-CN',
-  'zh-TW',
-  'fr',
-  'de',
-  'ja',
-  'es',
-  'ca',
-  'el',
-  'pt',
-  'it',
-  'tr',
-  'ru',
-  'nl',
-  'hr',
-  'pl',
-  'uk',
-  'hi',
-  'sv',
-  'eo',
-  'da',
-  'ko',
-  'id',
-  'sr',
-  'vi',
-  'ar',
-  'cs',
-  'sk'
-]
+export const availableLanguages = () => ['en']
