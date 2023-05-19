9ca6485219f12181efed4688ce16509277096ac6
Tilman Vatteroth <git@tilmanvatteroth.de>
2023-05-19 14:22:10 +0200

    refactor: extract permission metadata key
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- backend/src/permissions/require-permission.decorator.ts
+++ backend/src/permissions/require-permission.decorator.ts
@@ -7,12 +7,13 @@ import { CustomDecorator, SetMetadata } from '@nestjs/common';
 
 import { RequiredPermission } from './required-permission.enum';
 
+export const PERMISSION_METADATA_KEY = 'requiredPermission';
+
 /**
- * This decorator gathers the {@link Permission Permission} a user must hold for the {@link PermissionsGuard}
- * @param permissions - an array of permissions. In practice this should always contain exactly one {@link Permission}
- * @constructor
+ * This decorator gathers the {@link RequiredPermission Permission} a user must hold for the {@link PermissionsGuard}
+ * @param permissions - an array of permissions. In practice this should always contain exactly one {@link RequiredPermission}
  */
 // eslint-disable-next-line func-style,@typescript-eslint/naming-convention
 export const RequirePermission = (
   ...permissions: RequiredPermission[]
-): CustomDecorator => SetMetadata('permissions', permissions);
+): CustomDecorator => SetMetadata(PERMISSION_METADATA_KEY, permissions);
