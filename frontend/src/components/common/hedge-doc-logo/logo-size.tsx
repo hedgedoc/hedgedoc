1c564b147985ceac7dd4da5481c2f9ff35377c11
Tilman Vatteroth <git@tilmanvatteroth.de>
2023-06-01 17:27:33 +0200

    refactor: replace logo svgs with components
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

---
+++ frontend/src/components/common/hedge-doc-logo/logo-size.tsx
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export enum LogoSize {
+  SMALL = 32,
+  MEDIUM = 64,
+  BIG = 256
+}
--- frontend/src/components/editor-page/app-bar/navbar-branding.tsx
+++ frontend/src/components/editor-page/app-bar/navbar-branding.tsx
@@ -5,11 +5,8 @@
  */
 import { useDarkModeState } from '../../../hooks/dark-mode/use-dark-mode-state'
 import { CustomBranding } from '../../common/custom-branding/custom-branding'
-import {
-  HedgeDocLogoSize,
-  HedgeDocLogoType,
-  HedgeDocLogoWithText
-} from '../../common/hedge-doc-logo/hedge-doc-logo-with-text'
+import { HedgeDocLogoHorizontalGrey } from '../../common/hedge-doc-logo/hedge-doc-logo-horizontal-grey'
+import { LogoSize } from '../../common/hedge-doc-logo/logo-size'
 import { BrandingSeparatorDash } from './branding-separator-dash'
 import Link from 'next/link'
 import React from 'react'
@@ -24,10 +21,7 @@ export const NavbarBranding: React.FC = () => {
   return (
     <Navbar.Brand>
       <Link href='/intro' className='text-secondary text-decoration-none d-flex align-items-center'>
-        <HedgeDocLogoWithText
-          logoType={darkModeActivated ? HedgeDocLogoType.WB_HORIZONTAL : HedgeDocLogoType.BW_HORIZONTAL}
-          size={HedgeDocLogoSize.SMALL}
-        />
+        <HedgeDocLogoHorizontalGrey size={LogoSize.SMALL} color={darkModeActivated ? 'dark' : 'light'} />
         <BrandingSeparatorDash />
         <CustomBranding inline={true} />
       </Link>
