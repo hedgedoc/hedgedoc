30b374ee5d6ec0ea1cd78a60eb969d12176b6c73
Tilman Vatteroth <git@tilmanvatteroth.de>
2023-06-29 22:19:19 +0200

    fix: better apple keyboard detection
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- frontend/src/components/editor-page/utils.ts
+++ frontend/src/components/editor-page/utils.ts
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
+ * SPDX-FileCopyrightText: 2023 The HedgeDoc developers (see AUTHORS file)
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
@@ -9,4 +9,5 @@ import { isClientSideRendering } from '../../utils/is-client-side-rendering'
  * Determines if the client is running on a Mac.
  * This is necessary to e.g. determine different keyboard shortcuts.
  */
-export const isMac: () => boolean = () => isClientSideRendering() && navigator.platform.toLowerCase().includes('mac')
+export const isMac: () => boolean = () =>
+  isClientSideRendering() && (navigator.platform.startsWith('Mac') || navigator.platform === 'iPhone')
