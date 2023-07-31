46e1b7471d6f04a2ae285fe81e60125755c05e60
Tilman Vatteroth <git@tilmanvatteroth.de>
2023-07-31 20:53:24 +0200

    refactor: rename isMac to isAppleDevice
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

---
+++ frontend/src/additional-global-types.d.ts
@@ -0,0 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+/// <reference types="user-agent-data-types" />
--- frontend/src/components/global-dialogs/shortcuts-modal/modifier-key.tsx
+++ frontend/src/components/global-dialogs/shortcuts-modal/modifier-key.tsx
@@ -3,12 +3,12 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import { isMac } from '../../editor-page/utils'
+import { isAppleDevice } from '../../../utils/is-apple-device'
 import React from 'react'
 
 /**
  * Renders a keyboard control/command key hint depending on if the browser is running on macOS or not.
  */
 export const ModifierKey: React.FC = () => {
-  return isMac() ? <kbd>⌘</kbd> : <kbd>Ctrl</kbd>
+  return isAppleDevice() ? <kbd>⌘</kbd> : <kbd>Ctrl</kbd>
 }
