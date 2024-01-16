5b672e3010f9cc1c51d38aafbb21aa12a9f5fb52
yamashush <38120991+yamashush@users.noreply.github.com>
2024-01-16 21:34:34 +0900

    test: add test case when config value is zero
    Signed-off-by: yamashush <38120991+yamashush@users.noreply.github.com>

--- backend/src/revisions/revisions.service.spec.ts
+++ backend/src/revisions/revisions.service.spec.ts
@@ -537,5 +537,13 @@ describe('RevisionsService', () => {
       await service.removeOldRevisions();
       expect(spyOnRemove).toHaveBeenCalledTimes(0);
     });
+
+    it('do nothing when retention days config is zero', async () => {
+      revisionConfig.retentionDays = 0;
+      const spyOnRemove = jest.spyOn(revisionRepo, 'remove')
+
+      await service.removeOldRevisions();
+      expect(spyOnRemove).toHaveBeenCalledTimes(0);
+    });
   });
 });
