7e08a0fada13c3febe6a5ea34922447744cf8024
yamashush <38120991+yamashush@users.noreply.github.com>
2024-01-18 19:30:09 +0900

    change not to update state object
    Signed-off-by: yamashush <38120991+yamashush@users.noreply.github.com>

--- frontend/src/components/editor-page/sidebar/specific-sidebar-entries/aliases-sidebar-entry/aliases-modal/aliases-list.tsx
+++ frontend/src/components/editor-page/sidebar/specific-sidebar-entries/aliases-sidebar-entry/aliases-modal/aliases-list.tsx
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { useApplicationState } from '../../../../../../hooks/common/use-application-state'
+import type { Alias } from '../../../../../../api/alias/types'
 import type { ApplicationState } from '../../../../../../redux/application-state'
 import { AliasesListEntry } from './aliases-list-entry'
 import React, { Fragment, useMemo } from 'react'
@@ -13,13 +14,12 @@ import React, { Fragment, useMemo } from 'react'
  */
 export const AliasesList: React.FC = () => {
   const aliases = useApplicationState((state: ApplicationState) => state.noteDetails?.aliases)
-
   const aliasesDom = useMemo(() => {
     return aliases === undefined
       ? null
-      : aliases
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map((alias) => <AliasesListEntry alias={alias} key={alias.name} />)
+      : Object.assign([], aliases)
+          .sort((a: Alias, b: Alias) => a.name.localeCompare(b.name))
+          .map((alias: Alias) => <AliasesListEntry alias={alias} key={alias.name} />)
   }, [aliases])
 
   return <Fragment>{aliasesDom}</Fragment>
