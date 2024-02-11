c3d61c88d30517bbdb602ee9cc70a442946c0af5
Erik Michelson <github@erik.michelson.eu>
2024-02-12 00:39:00 +0100

    fix(types): fix types for markdown-it-emoji
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

--- frontend/src/extensions/essential-app-extensions/emoji/emoji-markdown-renderer-extension.ts
+++ frontend/src/extensions/essential-app-extensions/emoji/emoji-markdown-renderer-extension.ts
@@ -1,12 +1,12 @@
 /*
- * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
+ * SPDX-FileCopyrightText: 2024 The HedgeDoc developers (see AUTHORS file)
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { MarkdownRendererExtension } from '../../../components/markdown-renderer/extensions/_base-classes/markdown-renderer-extension'
 import { combinedEmojiData } from './mapping'
 import type MarkdownIt from 'markdown-it'
-import emoji from 'markdown-it-emoji/bare'
+import { bare as emoji } from 'markdown-it-emoji'
 
 /**
  * Adds support for utf-8 emojis.
