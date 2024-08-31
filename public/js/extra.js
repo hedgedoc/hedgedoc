1b9123290956d15da2841ce27f9f994a898f5d13
David Mehren <git@herrmehren.de>
2024-08-31 18:51:16 +0200

    fix: anchor links
    Signed-off-by: David Mehren <git@herrmehren.de>

--- public/js/extra.js
+++ public/js/extra.js
@@ -847,7 +847,7 @@ const anchorForId = id => {
   const anchor = document.createElement('a')
   anchor.ariaHidden = 'true'
   anchor.className = 'anchor hidden-xs'
-  anchor.href = `#${id}`
+  anchor.href = `${document.location.origin}${document.location.pathname}#${id}`
   anchor.innerHTML = '<i class="fa fa-link"></i>'
   anchor.title = id
   return anchor
