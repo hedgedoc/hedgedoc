d84171219b4c7d2b217c157ad191f66b1ddf486b
yamashush <38120991+yamashush@users.noreply.github.com>
2024-09-27 08:23:58 +0900

    Use snapshot file
    Signed-off-by: yamashush <38120991+yamashush@users.noreply.github.com>
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

--- frontend/src/components/editor-page/sidebar/specific-sidebar-entries/revisions-sidebar-entry/revisions-modal/invert-unified-patch.spec.ts
+++ frontend/src/components/editor-page/sidebar/specific-sidebar-entries/revisions-sidebar-entry/revisions-modal/invert-unified-patch.spec.ts
@@ -18,44 +18,6 @@ describe('invert unified patch', () => {
 +d
  e`)[0]
     const result = invertUnifiedPatch(parsedPatch)
-    expect(result).toMatchInlineSnapshot(`
-      {
-        "hunks": [
-          {
-            "linedelimiters": [
-              "
-      ",
-              "
-      ",
-              "
-      ",
-              "
-      ",
-              "
-      ",
-              "
-      ",
-            ],
-            "lines": [
-              "+a",
-              "+b",
-              " c",
-              " d",
-              "-d",
-              " e",
-            ],
-            "newLines": 5,
-            "newStart": 1,
-            "oldLines": 4,
-            "oldStart": 1,
-          },
-        ],
-        "index": undefined,
-        "newFileName": "a",
-        "newHeader": "2022-07-03 21:21:07.499933337 +0200",
-        "oldFileName": "b",
-        "oldHeader": "2022-07-03 21:22:28.650972217 +0200",
-      }
-    `)
+    expect(result).toMatchSnapshot()
   })
 })
