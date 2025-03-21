1bb0c5eb2c1c11c7e19d9398f291aa2e9fae187b
Philip Molares <philip.molares@udo.edu>
2025-03-21 23:24:07 +0100

    refactor(backend): remove extra dot in base dto filename
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- backend/src/utils/base.dto.ts
+++ backend/src/utils/base.dto.ts
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022 The HedgeDoc developers (see AUTHORS file)
+ * SPDX-FileCopyrightText: 2025 The HedgeDoc developers (see AUTHORS file)
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
