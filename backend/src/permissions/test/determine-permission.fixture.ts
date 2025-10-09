621dfa36b928ef61e0ea9e09e58ddcbace251e1d
Erik Michelson <github@erik.michelson.eu>
2025-10-09 02:16:09 +0200

    chore(license): add missing license declaration to new file
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

--- backend/src/permissions/test/determine-permission.fixture.ts
+++ backend/src/permissions/test/determine-permission.fixture.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 import { PermissionLevel } from '@hedgedoc/commons';
 
 interface DeterminePermissionTestCase {
