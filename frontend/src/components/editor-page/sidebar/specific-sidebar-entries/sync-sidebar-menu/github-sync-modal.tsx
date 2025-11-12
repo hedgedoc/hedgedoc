8aa8e923b2984cfc3614e63e3e1d0a9304ee309a
EricChiu147 <88593071+EricChiu147@users.noreply.github.com>
2025-11-13 04:54:18 +0800

    feat: add token window

--- frontend/src/components/editor-page/sidebar/specific-sidebar-entries/sync-sidebar-menu/github-sync-modal.tsx
+++ frontend/src/components/editor-page/sidebar/specific-sidebar-entries/sync-sidebar-menu/github-sync-modal.tsx
@@ -3,11 +3,14 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import type { ModalVisibilityProps } from '../../../../common/modals/common-modal'
 import { CommonModal } from '../../../../common/modals/common-modal'
 import { Github } from 'react-bootstrap-icons'
-import { Modal } from 'react-bootstrap'
+import { Button, FormControl, FormGroup, FormLabel, FormText, Modal } from 'react-bootstrap'
+import { ExternalLink } from '../../../../common/links/external-link'
+import { useOnInputChange } from '../../../../../hooks/common/use-on-input-change'
+import { validateToken } from '../export-sidebar-menu/entries/export-gist-sidebar-entry/validate-token'
 
 /**
  * Placeholder modal for configuring GitHub sync.
@@ -16,9 +19,31 @@ import { Modal } from 'react-bootstrap'
  * @param onHide Callback that is fired when the modal is about to be closed.
  */
 export const GithubSyncModal: React.FC<ModalVisibilityProps> = ({ show, onHide }) => {
+  const [ghToken, setGhToken] = useState('')
+  const onGhTokenChange = useOnInputChange(setGhToken)
+  const ghTokenFormatValid = useMemo(() => validateToken(ghToken), [ghToken])
+
   return (
-    <CommonModal show={show} onHide={onHide} showCloseButton={true} titleIcon={Github} title={'Github'}>
-      <Modal.Body>{/* Placeholder content for future GitHub sync settings */}</Modal.Body>
+    <CommonModal show={show} onHide={onHide} showCloseButton={true} titleIcon={Github} title={'Github Sync'}>
+      <Modal.Body>
+        <h5 className={'mb-2'}>Authentication</h5>
+        <FormGroup className={'my-2'}>
+          <FormLabel>GitHub Token</FormLabel>
+          <FormControl value={ghToken} onChange={onGhTokenChange} type={'password'} isInvalid={!ghTokenFormatValid} />
+          <FormText muted={true}>
+            Create a token:{' '}
+            <ExternalLink
+              text={'https://github.com/settings/personal-access-tokens/new'}
+              href={'https://github.com/settings/personal-access-tokens/new'}
+            />
+          </FormText>
+        </FormGroup>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant={'success'} disabled={!ghTokenFormatValid}>
+          Next
+        </Button>
+      </Modal.Footer>
     </CommonModal>
   )
 }
