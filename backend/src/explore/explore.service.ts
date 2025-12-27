dabc5da3c3765d13e624d3c2a24b4e0847cf9833
Erik Michelson <github@erik.michelson.eu>
2025-12-28 00:14:13 +0100

    refactor(explore): extract tags from common queries
    Co-authored-by: Philip Molares <philip.molares@udo.edu>
    Signed-off-by: Philip Molares <philip.molares@udo.edu>
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

--- backend/src/explore/explore.service.ts
+++ backend/src/explore/explore.service.ts
@@ -14,6 +14,7 @@ import {
   FieldNameUser,
   FieldNameUserPinnedNote,
   FieldNameVisitedNote,
+  Note,
   SpecialGroup,
   TableAlias,
   TableNote,
@@ -33,6 +34,7 @@ import { InjectConnection } from 'nest-knexjs';
 import { NoteExploreEntryDto } from '../dtos/note-explore-entry.dto';
 import { GroupsService } from '../groups/groups.service';
 import { ConsoleLoggerService } from '../logger/console-logger.service';
+import { RevisionsService } from '../revisions/revisions.service';
 
 export const ENTRIES_PER_PAGE_LIMIT = 20;
 
@@ -44,10 +46,9 @@ interface QueryResult {
   lastChangedAt: string;
   lastVisitedAt: string | null;
   revisionUuid: string;
-  tag: string;
 }
 
-interface QueryResultWithTagList extends Omit<QueryResult, 'tag'> {
+interface QueryResultWithTagList extends QueryResult {
   tags: string[];
 }
 
@@ -58,6 +59,7 @@ export class ExploreService {
     @InjectConnection()
     private readonly knex: Knex,
     private readonly groupsService: GroupsService,
+    private readonly revisionsService: RevisionsService,
   ) {
     this.logger.setContext(ExploreService.name);
   }
@@ -80,24 +82,26 @@ export class ExploreService {
     sortBy?: OptionalSortMode,
     search?: string,
   ): Promise<NoteExploreEntryDto[]> {
-    const queryBase = this.knex(TableNote);
-    let query = this.applyCommonQuery(queryBase);
-    if (
-      sortBy === SortMode.LAST_VISITED_ASC ||
-      sortBy === SortMode.LAST_VISITED_DESC
-    ) {
-      query = this.joinWithTableVisitedNote(query);
-    }
-    query = query.andWhere(`${TableNote}.${FieldNameNote.ownerId}`, userId);
-    query = this.applyFiltersToQuery(query, noteType, search);
-    query = this.applySortingToQuery(query, sortBy);
-    query = this.applyPaginationToQuery(query, page);
-    const results = (await query) as QueryResult[];
-    return this.transformQueryResultIntoDtos(results);
+    return await this.knex.transaction(async (transaction) => {
+      const queryBase = transaction(TableNote);
+      let query = this.applyCommonQuery(queryBase, transaction);
+      if (
+        sortBy === SortMode.LAST_VISITED_ASC ||
+        sortBy === SortMode.LAST_VISITED_DESC
+      ) {
+        query = this.joinWithTableVisitedNote(query);
+      }
+      query = query.andWhere(`${TableNote}.${FieldNameNote.ownerId}`, userId);
+      query = this.applyFiltersToQuery(query, noteType, search);
+      query = this.applySortingToQuery(query, sortBy);
+      query = this.applyPaginationToQuery(query, page);
+      const results = (await query) as QueryResult[];
+      return this.transformQueryResultIntoDtos(results, transaction);
+    });
   }
 
   /**
-   * Return a list of {@link NoteExploreEntryDto} that are explicity shared with the user.
+   * Return a list of {@link NoteExploreEntryDto} that are explicitly shared with the user.
    *
    * @param userId The user that the note is shared with
    * @param page Which page of the list is wanted
@@ -135,6 +139,29 @@ export class ExploreService {
     query = this.applyPaginationToQuery(query, page);
     const results = (await query) as QueryResult[];
     return this.transformQueryResultIntoDtos(results);
+    return await this.knex.transaction(async (transaction) => {
+      const queryBase = transaction(TableNoteUserPermission).join(
+        TableNote,
+        `${TableNoteUserPermission}.${FieldNameNoteUserPermission.noteId}`,
+        `${TableNote}.${FieldNameNote.id}`,
+      );
+      let query = this.applyCommonQuery(queryBase, transaction);
+      if (
+        sortBy === SortMode.LAST_VISITED_ASC ||
+        sortBy === SortMode.LAST_VISITED_DESC
+      ) {
+        query = this.joinWithTableVisitedNote(query);
+      }
+      query = query.andWhere(
+        `${TableNoteUserPermission}.${FieldNameNoteUserPermission.userId}`,
+        userId,
+      );
+      query = this.applyFiltersToQuery(query, noteType, search);
+      query = this.applySortingToQuery(query, sortBy);
+      query = this.applyPaginationToQuery(query, page);
+      const results = (await query) as QueryResult[];
+      return this.transformQueryResultIntoDtos(results, transaction);
+    });
   }
 
   /**
@@ -156,29 +183,31 @@ export class ExploreService {
     const everyoneGroupId = await this.groupsService.getGroupIdByName(
       SpecialGroup.EVERYONE,
     );
-    const queryBase = this.knex(TableNote).join(
-      TableNoteGroupPermission,
-      `${TableNote}.${FieldNameNote.id}`,
-      `${TableNoteGroupPermission}.${FieldNameNoteGroupPermission.noteId}`,
-    );
-    let query = this.applyCommonQuery(queryBase);
-    query = query
-      .andWhere(
-        `${TableNoteGroupPermission}.${FieldNameNoteGroupPermission.groupId}`,
-        everyoneGroupId,
-      )
-      .andWhere(`${TableNote}.${FieldNameNote.publiclyVisible}`, true);
-    if (
-      sortBy === SortMode.LAST_VISITED_ASC ||
-      sortBy === SortMode.LAST_VISITED_DESC
-    ) {
-      query = this.joinWithTableVisitedNote(query);
-    }
-    query = this.applyFiltersToQuery(query, noteType, search);
-    query = this.applySortingToQuery(query, sortBy);
-    query = this.applyPaginationToQuery(query, page);
-    const results = (await query) as QueryResult[];
-    return this.transformQueryResultIntoDtos(results);
+    return await this.knex.transaction(async (transaction) => {
+      const queryBase = transaction(TableNote).join(
+        TableNoteGroupPermission,
+        `${TableNote}.${FieldNameNote.id}`,
+        `${TableNoteGroupPermission}.${FieldNameNoteGroupPermission.noteId}`,
+      );
+      let query = this.applyCommonQuery(queryBase, transaction);
+      query = query
+        .andWhere(
+          `${TableNoteGroupPermission}.${FieldNameNoteGroupPermission.groupId}`,
+          everyoneGroupId,
+        )
+        .andWhere(`${TableNote}.${FieldNameNote.publiclyVisible}`, true);
+      if (
+        sortBy === SortMode.LAST_VISITED_ASC ||
+        sortBy === SortMode.LAST_VISITED_DESC
+      ) {
+        query = this.joinWithTableVisitedNote(query);
+      }
+      query = this.applyFiltersToQuery(query, noteType, search);
+      query = this.applySortingToQuery(query, sortBy);
+      query = this.applyPaginationToQuery(query, page);
+      const results = (await query) as QueryResult[];
+      return this.transformQueryResultIntoDtos(results, transaction);
+    });
   }
 
   /**
@@ -191,19 +220,21 @@ export class ExploreService {
   async getMyPinnedNoteExploreEntries(
     userId: number,
   ): Promise<NoteExploreEntryDto[]> {
-    const queryBase = this.knex(TableUserPinnedNote).join(
-      TableNote,
-      `${TableUserPinnedNote}.${FieldNameUserPinnedNote.noteId}`,
-      `${TableNote}.${FieldNameNote.id}`,
-    );
-    let query = this.applyCommonQuery(queryBase);
-    query = query.andWhere(
-      `${TableUserPinnedNote}.${FieldNameUserPinnedNote.userId}`,
-      userId,
-    );
-    query = this.applySortingToQuery(query, SortMode.UPDATED_AT_DESC);
-    const results = (await query) as QueryResult[];
-    return this.transformQueryResultIntoDtos(results);
+    return await this.knex.transaction(async (transaction) => {
+      const queryBase = transaction(TableUserPinnedNote).join(
+        TableNote,
+        `${TableUserPinnedNote}.${FieldNameUserPinnedNote.noteId}`,
+        `${TableNote}.${FieldNameNote.id}`,
+      );
+      let query = this.applyCommonQuery(queryBase, transaction);
+      query = query.andWhere(
+        `${TableUserPinnedNote}.${FieldNameUserPinnedNote.userId}`,
+        userId,
+      );
+      query = this.applySortingToQuery(query, SortMode.UPDATED_AT_DESC);
+      const results = (await query) as QueryResult[];
+      return this.transformQueryResultIntoDtos(results, transaction);
+    });
   }
 
   /**
@@ -228,42 +259,42 @@ export class ExploreService {
     let query = this.applyCommonQuery(queryBase);
     query = query.select({
       lastVisitedAt: `${TableVisitedNote}.${FieldNameVisitedNote.visitedAt}`,
+    return await this.knex.transaction(async (transaction) => {
+      const queryBase = transaction(TableNote);
+      let query = this.applyCommonQuery(queryBase, transaction);
+      query = query.select({
+        lastVisitedAt: `${TableVisitedNote}.${FieldNameVisitedNote.visitedAt}`,
+      });
+      query = this.joinWithTableVisitedNote(query);
+      query = query.andWhere(
+        `${TableVisitedNote}.${FieldNameVisitedNote.userId}`,
+        userId,
+      );
+      query = this.applyFiltersToQuery(query, noteType, search);
+      query = this.applySortingToQuery(query, sortBy);
+      query = this.applyPaginationToQuery(query, page);
+      const results = (await query) as (QueryResult &
+        Pick<Note, FieldNameNote.id>)[];
+      return this.transformQueryResultIntoDtos(results, transaction);
     });
-    query = this.joinWithTableVisitedNote(query);
-    query = query.andWhere(
-      `${TableVisitedNote}.${FieldNameVisitedNote.userId}`,
-      userId,
-    );
-    query = this.applyFiltersToQuery(query, noteType, search);
-    query = this.applySortingToQuery(query, sortBy);
-    query = this.applyPaginationToQuery(query, page);
-    const results = (await query) as QueryResult[];
-    return this.transformQueryResultIntoDtos(results);
   }
 
-  private transformQueryResultIntoDtos(
+  private async transformQueryResultIntoDtos(
     results: QueryResult[],
-  ): NoteExploreEntryDto[] {
-    const resultsWithTagList = results.reduce((map, result) => {
-      const existing = map.get(result.primaryAlias);
-      if (!existing) {
-        map.set(result.primaryAlias, {
-          primaryAlias: result.primaryAlias,
-          title: result.title,
-          noteType: result.noteType,
-          ownerUsername: result.ownerUsername,
-          lastChangedAt: result.lastChangedAt,
-          lastVisitedAt: result.lastVisitedAt ?? null,
-          revisionUuid: result.revisionUuid,
-          tags: result.tag ? [result.tag] : [],
-        });
-      } else if (result.tag) {
-        existing.tags.push(result.tag);
-      }
-      return map;
-    }, new Map<string, QueryResultWithTagList>());
-
-    return Array.from(resultsWithTagList.values()).map((result) =>
+    transaction: Knex.Transaction,
+  ): Promise<NoteExploreEntryDto[]> {
+    const resultsWithTagList: QueryResultWithTagList[] = [];
+    const tagsPromises = results.map((result) =>
+      this.revisionsService.getTagsByRevisionUuid(
+        result.revisionUuid,
+        transaction,
+      ),
+    );
+    const tags = await Promise.all(tagsPromises);
+    for (let i = 0; i < results.length; i++) {
+      resultsWithTagList.push({ ...results[i], tags: tags[i] });
+    }
+    return resultsWithTagList.map((result) =>
       NoteExploreEntryDto.create({
         primaryAlias: result.primaryAlias,
         title: result.title,
@@ -293,8 +324,11 @@ export class ExploreService {
 
   // The correct return type with all joins and selects is very specific and should just be inferred from Knex
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  private applyCommonQuery(query: Knex.QueryBuilder) {
-    const subquery = this.knex(TableRevision)
+  private applyCommonQuery(
+    query: Knex.QueryBuilder,
+    transaction: Knex.Transaction,
+  ) {
+    const subquery = transaction(TableRevision)
       .select(`${FieldNameRevision.uuid}`, `${FieldNameRevision.noteId}`)
       .rowNumber('rn', function () {
         void this.orderBy(FieldNameRevision.createdAt, 'desc').partitionBy(
@@ -310,7 +344,6 @@ export class ExploreService {
         createdAt: `${TableNote}.${FieldNameNote.createdAt}`,
         lastChangedAt: `${TableRevision}.${FieldNameRevision.createdAt}`,
         revisionUuid: `${TableRevision}.${FieldNameRevision.uuid}`,
-        tag: `${TableRevisionTag}.${FieldNameRevisionTag.tag}`,
       })
       .join(
         TableAlias,
@@ -344,11 +377,6 @@ export class ExploreService {
           `latest_revision.${FieldNameRevision.uuid}`,
         );
       })
-      .leftJoin(
-        TableRevisionTag,
-        `${TableRevisionTag}.${FieldNameRevisionTag.revisionUuid}`,
-        `latest_revision.${FieldNameRevision.uuid}`,
-      )
       .where(`${TableAlias}.${FieldNameAlias.isPrimary}`, true);
   }
 
@@ -442,33 +470,37 @@ export class ExploreService {
     noteId: number,
     isPinned: boolean,
   ): Promise<NoteExploreEntryDto | null> {
-    if (isPinned) {
-      // If note is already pinned, ignore that
-      await this.knex(TableUserPinnedNote)
-        .insert({
-          [FieldNameUserPinnedNote.userId]: userId,
-          [FieldNameUserPinnedNote.noteId]: noteId,
-        })
-        .onConflict([
-          FieldNameUserPinnedNote.userId,
-          FieldNameUserPinnedNote.noteId,
-        ])
-        .ignore();
-      const queryBase = this.knex(TableNote).where(
-        `${TableNote}.${FieldNameNote.id}`,
-        noteId,
-      );
-      const query = this.applyCommonQuery(queryBase);
-      const results = (await query) as QueryResult[];
-      return this.transformQueryResultIntoDtos(results)[0];
-    } else {
-      await this.knex(TableUserPinnedNote)
-        .where({
-          [FieldNameUserPinnedNote.userId]: userId,
-          [FieldNameUserPinnedNote.noteId]: noteId,
-        })
-        .delete();
-      return null;
-    }
+    return await this.knex.transaction(async (transaction) => {
+      if (isPinned) {
+        // If the note is already pinned, ignore that
+        await transaction(TableUserPinnedNote)
+          .insert({
+            [FieldNameUserPinnedNote.userId]: userId,
+            [FieldNameUserPinnedNote.noteId]: noteId,
+          })
+          .onConflict([
+            FieldNameUserPinnedNote.userId,
+            FieldNameUserPinnedNote.noteId,
+          ])
+          .ignore();
+        const queryBase = transaction(TableNote).where(
+          `${TableNote}.${FieldNameNote.id}`,
+          noteId,
+        );
+        const query = this.applyCommonQuery(queryBase, transaction);
+        const results = (await query) as QueryResult[];
+        return (
+          await this.transformQueryResultIntoDtos(results, transaction)
+        )[0];
+      } else {
+        await transaction(TableUserPinnedNote)
+          .where({
+            [FieldNameUserPinnedNote.userId]: userId,
+            [FieldNameUserPinnedNote.noteId]: noteId,
+          })
+          .delete();
+        return null;
+      }
+    });
   }
 }
