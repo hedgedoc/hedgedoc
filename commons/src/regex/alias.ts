e12a4f78dc96813733cdd3d59cd0808974dbf7e5
Erik Michelson <github@erik.michelson.eu>
2026-02-18 22:27:45 +0100

    fix(aliases): verify alias regex in frontend and backend
    Co-authored-by: Philip Molares <philip.molares@udo.edu>
    Signed-off-by: Philip Molares <philip.molares@udo.edu>
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

---
+++ commons/src/regex/alias.ts
@@ -0,0 +1,6 @@
+/*
+ * SPDX-FileCopyrightText: 2026 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+export const ALIAS_REGEX = /^[\p{Letter}\p{Mark}\p{Number}_\-\p{RGI_Emoji}]+$/v
