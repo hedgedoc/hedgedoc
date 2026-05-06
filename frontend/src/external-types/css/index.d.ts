bfce499a2e1a40537abbd36782cfbc5f74e15bd5
Erik Michelson <github@erik.michelson.eu>
2026-05-06 20:57:17 +0200

    chore(lint): enable TypeScript linting in frontend
    oxlint now includes support for TypeScript-aware linting by using
    the oxlint-tsgolint package. While this increases the chance of
    finding bugs early, it requires a few changes to the TypeScript
    configuration, especially the explicit declaration of CSS imports
    and changing the moduleResolution to bundler mode.
    
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

---
+++ frontend/src/external-types/css/index.d.ts
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2026 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+declare module '*.css'
+declare module '*.scss'
+
+declare module '*.module.css' {
+  const classes: Record<string, string>
+  export default classes
+}
+
+declare module '*.module.scss' {
+  const classes: Record<string, string>
+  export default classes
+}
