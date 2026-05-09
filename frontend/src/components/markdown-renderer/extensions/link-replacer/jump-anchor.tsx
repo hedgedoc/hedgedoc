9757171f8c19dbb14f6d8c368a5b225af6311c89
Erik Michelson <github@erik.michelson.eu>
2026-05-09 03:25:17 +0200

    fix(frontend): updates to test files after linting changes
    Signed-off-by: Erik Michelson <github@erik.michelson.eu>

--- frontend/src/components/markdown-renderer/extensions/link-replacer/jump-anchor.tsx
+++ frontend/src/components/markdown-renderer/extensions/link-replacer/jump-anchor.tsx
@@ -34,7 +34,7 @@ export const JumpAnchor: React.FC<JumpAnchorProps> = ({ jumpTargetId, children, 
   )
 
   return (
-    <a {...props} onClick={jumpToTargetId} href={`#${jumpTargetId}`}>
+    <a {...props} onClick={jumpToTargetId} href={props.href ?? `#${jumpTargetId}`}>
       {children}
     </a>
   )
