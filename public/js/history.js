0f3723b4407bb9f176e192aa57fd86b8c68351f1
Philip Molares <philip.molares@udo.edu>
2026-06-22 23:06:17 +0200

    refactor: remove Temp
    This was a very old way of migrating the history and is no longer needed.
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- public/js/history.js
+++ public/js/history.js
@@ -3,7 +3,6 @@
 
 import store from 'store'
 import LZString from 'lz-string'
-import url from 'wurl'
 
 import {
   checkNoteIdValid,
@@ -18,40 +17,6 @@ import {
 import {
   urlpath
 } from './lib/config'
-
-window.migrateHistoryFromTempCallback = null
-
-migrateHistoryFromTemp()
-
-function migrateHistoryFromTemp () {
-  if (url('#tempid')) {
-    $.get(`${serverurl}/temp`, {
-      tempid: url('#tempid')
-    })
-      .done(data => {
-        if (data && data.temp) {
-          getStorageHistory(olddata => {
-            if (!olddata || olddata.length === 0) {
-              saveHistoryToStorage(JSON.parse(data.temp))
-            }
-          })
-        }
-      })
-      .always(() => {
-        let hash = location.hash.split('#')[1]
-        hash = hash.split('&')
-        for (let i = 0; i < hash.length; i++) {
-          if (hash[i].indexOf('tempid') === 0) {
-            hash.splice(i, 1)
-            i--
-          }
-        }
-        hash = hash.join('&')
-        location.hash = hash
-        if (window.migrateHistoryFromTempCallback) { window.migrateHistoryFromTempCallback() }
-      })
-  }
-}
 
 export function saveHistory (notehistory) {
   checkIfAuth(
