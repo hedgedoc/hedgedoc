27f770e721318b4580f3fa22470cbc701ad01ec7
Philip Molares <philip.molares@udo.edu>
2026-09-19 15:28:39 +0200

    chore: fix 'use client' placement
    The 'use client' statement needs to come first in relevant files.
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- frontend/src/app/(editor)/profile/page.tsx
+++ frontend/src/app/(editor)/profile/page.tsx
@@ -1,13 +1,7 @@
-/*
- * SPDX-FileCopyrightText: 2025 The HedgeDoc developers (see AUTHORS file)
- *
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-
 'use client'
 
 /*
- SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ SPDX-FileCopyrightText: 2025 The HedgeDoc developers (see AUTHORS file)
 
  SPDX-License-Identifier: AGPL-3.0-only
  */
